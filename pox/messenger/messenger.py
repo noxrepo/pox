6a2168b85f4a0cc1bd845d8805eafb197daafb36
Murphy McCauley <murphy.mccauley@gmail.com>
2011-07-01 04:55:00 -0700

    Add big docstring for messenger example

--- pox/messenger/messenger.py
+++ pox/messenger/messenger.py
@@ -251,6 +251,22 @@ class TCPMessengerSource (Task):
 
 
 class MessengerExample (object):
+  """
+  A demo of messenger.
+
+  The idea is pretty simple. When you create a MessengerExample, you tell it a
+  name you're interested in. It listens to core.messenger!MessageRecieved. If
+  it sees a message with a "hello" key where the value is the name you're
+  interested in, it claims the connection that message came in on, and then
+  listens to <thatConnection>!MessageRecieved. It prints out messages on that
+  connection from then on. If a message has a key named "bye" with the value
+  True, it closes the connection
+
+  To try it out, do the following in the POX interpreter:
+  POX> pox.messenger.messenger.MessengerExample("foo")
+  And then do the following from the commandline:
+  bash$ echo '{"hello":"foo"}[1,2,3] "neat"' | nc localhost 7790
+  """
   def __init__ (self, targetName):
     core.messenger.addListener(MessageRecieved, self._handle_global_MessageRecieved, weak=True)
     self._targetName = targetName
