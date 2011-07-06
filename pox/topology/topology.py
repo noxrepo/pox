804009efe7ae92ea8bcad3f9e6650bc142dec328
Murphy McCauley <murphy.mccauley@gmail.com>
2011-07-06 00:53:17 -0700

    Topology: Started restructing towards the NOM
    First off, Topology shouldn't have to be OpenFlow-aware, and this is
    a first step in that direction.
    Secondly, I think Topology is the beginning of the Network Object Model
    (NOM), so it needs to be modeling various network entities.

--- pox/topology/topology.py
+++ pox/topology/topology.py
@@ -3,23 +3,36 @@ from pox.core import core
 
 log = core.getLogger()
 
-class DatapathEvent (Event):
-  def __init__ (self, datapath):
-    self.datapath = datapath
+class EntityEvent (Event):
+  def __init__ (self, entity):
+    self.entity = entity
 
-class SwitchJoin (DatapathEvent): pass
-class SwitchLeave (DatapathEvent): pass
+class SwitchEvent (EntityEvent): pass
+class SwitchJoin (SwitchEvent): pass
+class SwitchLeave (SwitchEvent): pass
 
-class HostEvent (Event):
-  def __init__ (self, host):
-    self.host = host
-
+class HostEvent (EntityEvent): pass
 class HostJoin (HostEvent): pass
 class HostLeave (HostEvent): pass
 
+class EntityJoin (EntityEvent): pass
+class EntityLeave (EntityEvent): pass
 
+class Entity (object):
+  def __init__ (self, id):
+    self.id = id
 
+class Host (Entity):
+  pass
 
+class Switch (Entity):
+  pass
+
+class Port (Entity):
+  def __init__ (self, num, hwAddr, name):
+    self.number = num
+    self.hwAddr = EthAddr(hwAddr)
+    self.name = name
 
 class Topology (EventMixin):
   _eventMixin_events = [
@@ -27,29 +40,33 @@ class Topology (EventMixin):
     SwitchLeave,
     HostJoin,
     HostLeave,
+    EntityJoin,
+    EntityLeave,
   ]
 
   def __init__ (self):
-    #EventMixin.__init__(self)
-    self.switches = {}
+    EventMixin.__init__(self)
+    self.entities = {}
 
-    if core.hasComponent("openflow"):
-      self.listenTo(core.openflow)
+  def getEntityByID (self, ID):
+    return self.entities[ID]
+
+  def addEntity (self, entity):
+    assert entity.id not in self.entities
+    self.entities[entity.id] = entity
+    log.info(str(entity) + " joined")
+    if isinstance(entity, Switch):
+      self.raiseEvent(SwitchJoin, entity)
+    elif isinstance(entity, Host):
+      self.raiseEvent(HostJoin, entity)
     else:
-      # We'll wait for openflow to come up
-      self.listenTo(core)
+      self.raiseEvent(EntityJoin, entity)
 
-  def _handle_ComponentRegistered (self, event):
-    if event.name is "openflow":
-      self.listenTo(event.component, prefix="OF01")
+  def getEntitiesOfType (self, t=Entity, subtypes=True):
+    if subtypes is False:
+      return (x for x in self.entities.itervalues() if type(x) is t)
+    else:
+      return (x for x in self.entities.itervalues() if isinstance(x, t))
 
-  def _handle_OF01_ConnectionUp (self, event):
-    if event.dpid in self.switches:
-      log.warn("Ignoring switch connection for %i because it's already connected" % (event.dpid,))
-      return EventHalt
-    self.switches[event.dpid] = event.connection
-    log.info("Switch " + str(event.dpid) + " connected")
 
-  def _handle_OF01_ConnectionDown (self, event):
-    log.info("Switch " + str(event.dpid) + " disconnected")
-    del self.switches[event.dpid]
+
