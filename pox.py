9547317f90b2fa45c8f2192c276f538c5d226cca
Kyriakos Zarifis <kyr.zarifis@gmail.com>
2011-07-18 00:36:03 -0700

    import GuiMessenger from right path

--- pox.py
+++ pox.py
@@ -12,12 +12,12 @@ import pox.openflow.discovery
 import pox.openflow.of_01
 import pox.dumb_l3_switch.dumb_l3_switch
 import pox.messenger.messenger
+from pox.gui_backend.guimessenger import GuiMessenger
 
 # Turn on extra info for event exceptions
 import pox.lib.revent.revent as revent
 revent.showEventExceptions = True
 
-from pox.messenger.guimessenger import GuiMessenger
 
 def startup ():
   core.register("openflow_topology", pox.openflow.openflowtopology.OpenFlowTopology())
