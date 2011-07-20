a2fca7f42d5e3162e8912450f9858fa6cc67ade2
Murphy McCauley <murphy.mccauley@gmail.com>
2011-07-19 19:19:04 -0700

    Made pox-gui.py executable; Fixed line endings
    pox-gui.py had Windows newlines: fixed
    Made shabang more portable

--- pox-gui.py
+++ pox-gui.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 '''
 NOX GUI
 This file creates the main application window, sets up the layout and invokes
@@ -6,7 +6,7 @@ the GUI's widgets.
 The left panel (log) is used for displaying context-specific information.
 The right panel (topology) is an interactive display of the topology
 The bottom right pane (console) is a frontend for communication with
-jsonmessenger. 
+jsonmessenger.
 
 @author Kyriakos Zarifis (kyr.zarifis@gmail.com)
 '''
@@ -24,9 +24,9 @@ import gui.console as console
 import gui.Popup as Popup
 import gui.settings as settings
 import signal
-       
+
 class MainWindow(QtGui.QMainWindow):
-    
+
     def usage(self):
         """Print usage information
         """
@@ -46,7 +46,7 @@ class MainWindow(QtGui.QMainWindow):
             print "Option error!"
             self.usage()
             sys.exit(2)
-            
+
         #Get options
         self.backend_port = 7790 #GuiMessenger port
         for opt,arg in opts:
@@ -64,21 +64,21 @@ class MainWindow(QtGui.QMainWindow):
             self.backend_ip = args[0]
         else:
             self.backend_ip = "127.0.0.1"
-            
+
         # Global Settings
         self.settings = settings.Settings(self)
 
         # Configure layout
         self.setWindowTitle('POX Graphical User Interface')
         self.resize(1280, 800)
-        self.statusBar().showMessage('Ready')        
+        self.statusBar().showMessage('Ready')
         self.center()
 
         self.logWidget = log.LogWidget(self)
-        self.infoWidget = info.InfoWidget(self) 
-        self.topoWidget = topology.TopoWidget(self) 
-        self.consoleWidget = console.ConsoleWidget(self) 
-        
+        self.infoWidget = info.InfoWidget(self)
+        self.topoWidget = topology.TopoWidget(self)
+        self.consoleWidget = console.ConsoleWidget(self)
+
         self.leftvbox = QtGui.QVBoxLayout()
         self.vSplitter = QtGui.QSplitter(QtCore.Qt.Vertical)
         self.vSplitter.addWidget(self.infoWidget)
@@ -88,77 +88,77 @@ class MainWindow(QtGui.QMainWindow):
         self.leftvbox.addWidget(self.vSplitter)
         self.left = QtGui.QWidget()
         self.left.setLayout(self.leftvbox)
-        
+
         self.rightvbox = QtGui.QVBoxLayout()
         self.rightvbox.addWidget(self.topoWidget)
         self.rightvbox.addWidget(self.consoleWidget)
         self.right = QtGui.QWidget()
         self.right.setLayout(self.rightvbox)
-        
+
         self.hSplitter = QtGui.QSplitter(QtCore.Qt.Horizontal)
         self.hSplitter.addWidget(self.left)
         self.hSplitter.addWidget(self.right)
-        
+
         self.setCentralWidget(self.hSplitter)
-        
-        signal.signal(signal.SIGINT, self.sigint_handler)  
+
+        signal.signal(signal.SIGINT, self.sigint_handler)
 
         # Actions
         start = QtGui.QAction(QtGui.QIcon('gui/icons/logo.png'), 'Start', self)
         start.setShortcut('Ctrl+S')
         start.setStatusTip('Start POX')
-        self.connect(start, QtCore.SIGNAL('triggered()'), self.start_nox)        
-        
+        self.connect(start, QtCore.SIGNAL('triggered()'), self.start_nox)
+
         switch_to_log = QtGui.QAction(QtGui.QIcon('gui/icons/log.png'),'Log View',self)
         switch_to_log.setShortcut('Ctrl+1')
         switch_to_log.setStatusTip('Switch to system log view')
         self.connect(switch_to_log, QtCore.SIGNAL('triggered()'), self.show_log)
-        
+
         switch_to_topo = QtGui.QAction(QtGui.QIcon('gui/icons/topo.png'),'Topology View',self)
         switch_to_topo.setShortcut('Ctrl+2')
         switch_to_topo.setStatusTip('Switch to topology view')
-        self.connect(switch_to_topo, QtCore.SIGNAL('triggered()'), self.show_topo)                
-                
+        self.connect(switch_to_topo, QtCore.SIGNAL('triggered()'), self.show_topo)
+
         switch_to_split = QtGui.QAction(QtGui.QIcon('gui/icons/split.png'),'Split View',self)
         switch_to_split.setShortcut('Ctrl+3')
         switch_to_split.setStatusTip('Switch to split view')
         self.connect(switch_to_split, QtCore.SIGNAL('triggered()'), self.show_split)
-        
+
         toggle_console = QtGui.QAction(QtGui.QIcon('gui/icons/split.png'),'Show/Hide Console',self)
         toggle_console.setShortcut('Ctrl+4')
         toggle_console.setStatusTip('Show/Hide Console')
         self.connect(toggle_console, QtCore.SIGNAL('triggered()'), self.toggle_show_console)
-        
+
         exit = QtGui.QAction(QtGui.QIcon('gui/icons/exit.png'), 'Exit', self)
         exit.setShortcut('Ctrl+Q')
         exit.setStatusTip('Exit application')
         self.connect(exit, QtCore.SIGNAL('triggered()'), QtCore.SLOT('close()'))
-        
+
         switch_to_dark = QtGui.QAction('Dark',self)
         switch_to_dark.setStatusTip('Switch to dark color theme')
-        self.connect(switch_to_dark, QtCore.SIGNAL('triggered()'), self.dark)     
-        
+        self.connect(switch_to_dark, QtCore.SIGNAL('triggered()'), self.dark)
+
         switch_to_bright = QtGui.QAction('Bright',self)
         switch_to_bright.setStatusTip('Switch to bright color theme')
-        self.connect(switch_to_bright, QtCore.SIGNAL('triggered()'), self.bright)        
-        
+        self.connect(switch_to_bright, QtCore.SIGNAL('triggered()'), self.bright)
+
         set_node_id_size_small = QtGui.QAction('Small',self)
         set_node_id_size_small.setStatusTip('Set node ID fonts to small')
         self.connect(set_node_id_size_small, QtCore.SIGNAL('triggered()'), \
-         self.settings.set_node_id_size_small) 
-        
+         self.settings.set_node_id_size_small)
+
         set_node_id_size_normal = QtGui.QAction('Normal',self)
         set_node_id_size_normal.setStatusTip('Set node ID fonts to small')
         self.connect(set_node_id_size_normal, QtCore.SIGNAL('triggered()'), \
-         self.settings.set_node_id_size_normal) 
-        
+         self.settings.set_node_id_size_normal)
+
         set_node_id_size_large = QtGui.QAction('Large',self)
         set_node_id_size_large.setStatusTip('Set node ID fonts to small')
         self.connect(set_node_id_size_large, QtCore.SIGNAL('triggered()'), \
-         self.settings.set_node_id_size_large) 
-        
+         self.settings.set_node_id_size_large)
+
         self.statusBar()
-       
+
         # Configure Menubar
         menubar = self.menuBar()
         file_menu = menubar.addMenu('&File')
@@ -192,16 +192,16 @@ class MainWindow(QtGui.QMainWindow):
         toolbar.addAction(switch_to_topo)
         toolbar.addAction(switch_to_split)
         toolbar.addAction(exit)
-                
+
     def center(self):
         screen = QtGui.QDesktopWidget().screenGeometry()
         size =  self.geometry()
         self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
-       
-    
+
+
     def sigint_handler(self, signal, frame):
-        sys.exit(0) 
-       
+        sys.exit(0)
+
     def closeEvent(self, event):
         '''
         reply = QtGui.QMessageBox.question(self, 'Exit NOX',
@@ -224,53 +224,53 @@ class MainWindow(QtGui.QMainWindow):
         """
         self.topoWidget.topologyView.topologyInterface.shutdown()
         self.logWidget.dbWrapper.closeDb()
-        
+
     def start_nox(self):
         popup = Popup.StartComboBox(self)
         popup.exec_()
-    
+
     def show_log(self):
         self.right.hide()
         self.left.show()
-        
+
     def show_topo(self):
         self.right.show()
         self.left.hide()
-        
+
     def show_split(self):
         self.right.show()
         self.left.show()
-    
+
     def dark(self):
         # Change Log colors
         self.logWidget.logDisplay.setStyleSheet("background-color: \
                 black; color: green")
-        
+
         # Change Topology colors
         self.topoWidget.topologyView.setStyleSheet("background: black")
         #self.topoWidget.topologyView.update()
         # stupid way to refresh background color:
         self.topoWidget.topologyView.scaleView(0.5)
         self.topoWidget.topologyView.scaleView(2)
-        
+
     def bright(self):
         # Change Topology colors
         self.logWidget.logDisplay.setStyleSheet("background-color: white; \
                 color: black")
-        
+
         # Change Topology colors
-        self.topoWidget.topologyView.setStyleSheet("background: white") 
+        self.topoWidget.topologyView.setStyleSheet("background: white")
         #self.topoWidget.topologyView.update()
         self.topoWidget.topologyView.scaleView(0.5)
         self.topoWidget.topologyView.scaleView(2)
-        
-    
+
+
     def toggle_show_console(self):
         if self.consoleWidget.isHidden():
             self.consoleWidget.show()
         else:
             self.consoleWidget.hide()
-                    
+
 app = QtGui.QApplication(sys.argv)
 app.setWindowIcon(QtGui.QIcon('gui/icons/logo.ico'))
 noxgui = MainWindow()
