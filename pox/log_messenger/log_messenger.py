c8f081f51fee9fa83fd580b8596ee7701cea0cf0
Kyriakos Zarifis <kyr.zarifis@gmail.com>
2011-07-21 04:04:22 -0700

    'message' value should be record.msg  (?)

--- pox/log_messenger/log_messenger.py
+++ pox/log_messenger/log_messenger.py
@@ -77,7 +77,7 @@ class LogMessenger (logging.Handler):
     logging.getLogger().removeHandler(self)
 
   def emit (self, record):
-    o = {'message' : self.format(record)}
+    o = {'message' : record.msg}
     if self._opaque is not None:
       o.update(self._opaque)
     #o['message'] = record.getMessage()
