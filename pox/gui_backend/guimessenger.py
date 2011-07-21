1761a3812a81699bb8e32411b543f02916e4dcdd
Murphy McCauley <murphy.mccauley@gmail.com>
2011-07-20 17:46:46 -0700

    gui_backend: Mostly rewrite.
    Each GUI session now has its own session object which, among other things,
    aggregates a LogMessenger so that the GUI can get log messages through the
    same messenger connection.  The messages from the logger get
    "source":"logger" added to them.  To send messages to the logger, send
    put "type":"logger" in your message.

--- pox/gui_backend/guimessenger.py
+++ pox/gui_backend/guimessenger.py
@@ -1,15 +1,6 @@
-"""
-Messenger works like this:
-
-Somehow a connection to a client is started.  pox.messenger raises a
-MessengerConnectEvent event.  If a listener wants the connection, it can call .claim()
-on the event, which returns a Connection.
-"""
-
 
 #from pox.lib.revent.revent import *
 #from pox.lib.recoco.recoco import *
-#import pox.core
 from pox.core import core as core
 from pox.messenger.messenger import MessageRecieved
 #import weakref
@@ -17,74 +8,83 @@ import json
 
 log = core.getLogger()
 
-class GuiMessenger (object):
-  def __init__ (self):
-    core.messenger.addListener(MessageRecieved, self._handle_global_MessageRecieved)
-    self._targetName = "gui"
-    log.info("Up!")
 
-  def _handle_global_MessageRecieved (self, event):
-    try:
-      n = event.con.read()['hello']
-      if n == self._targetName:
-        # It's for me!
-        event.claim()
-        event.con.addListener(MessageRecieved, self._handle_MessageRecieved)
-        log.info(self._targetName + " - started conversation with GUI")
-    except:
-      pass
+from pox.core import core
+from pox.messenger.messenger import *
+from pox.lib.revent.revent import *
+import traceback
+
+from pox.log_messenger.log_messenger import LogMessenger
+
+log = core.getLogger()
+
+class GuiMessengerService (EventMixin):
+  def __init__ (self, connection, params):
+    self.connection = connection
+    connection._newlines = params.get("newlines", True) == True #HACK
+
+    # Make LogMessenger always send back "source":"logger"
+    params['opaque'] = {'source':'logger'}
+    self._logService = LogMessenger(connection, params) # Aggregate
+    # Unhook its message received listener (we will pass it those events
+    # manually ourselves...)
+    connection.removeListener(dict(self._logService._listeners)[MessageRecieved])
+
+    self.listenTo(connection)
 
   def _handle_MessageRecieved (self, event):
     if event.con.isReadable():
       r = event.con.read()
-      log.info(self._targetName + " - " + str(r))
       if type(r) is dict:
-        if r.get("bye",False):
-          log.info(self._targetName + " - GOODBYE!")
+        self._processParameters(r)
+        if "bye" in r:
           event.con.close()
         else:
           # Dispatch message
           if r["type"] == "lavi":
-              log.info("got lavi")
+            log.info("got lavi")
           elif r["type"] == "monitoring":
-              log.info("got monitoring")
+            log.info("got monitoring")
           elif r["type"] == "spanning_tree":
-              log.info("got spanning_tree")
+            log.info("got spanning_tree")
           elif r["type"] == "sample_routing":
-              log.info("got sample_routing")
+            log.info("got sample_routing")
           elif r["type"] == "flowtracer":
-              log.info("got flowtracer")        
-    else:
-      log.info(self._targetName + " - conversation finished")
-      
+            log.info("got flowtracer")
+          elif r["type"] == "logging":
+            self._logService._processParameters(r)
+          else:
+            self.warn("Unknown type for message: %s", r)
 
-class GuiLogMessenger (object):
+
+class GuiMessengerServiceListener (object):
   def __init__ (self):
     core.messenger.addListener(MessageRecieved, self._handle_global_MessageRecieved)
-    self._targetName = "guilog"
-    print "guilogmessengerup!"
 
   def _handle_global_MessageRecieved (self, event):
     try:
-      n = event.con.read()['hello']
-      if n == self._targetName:
+      n = event.con.read()
+      if n['hello'] == 'gui':
         # It's for me!
-        event.claim()
-        event.con.addListener(MessageRecieved, self._handle_MessageRecieved)
-        log.info(self._targetName + " - started conversation with GUI log")
+        try:
+          GuiMessengerService(event.con, n)
+          event.claim()
+        except:
+          traceback.print_exc()
     except:
       pass
 
-  def _handle_MessageRecieved (self, event):
-    if event.con.isReadable():
-      r = event.con.read()
-      log.info(self._targetName + " - " + str(r))
-      if type(r) is dict:
-        if r.get("bye",False):
-          log.info(self._targetName + " - GOODBYE!")
-          event.con.close()
-        else:
-          # Dispatch message
-          pass
-    else:
-      log.info(self._targetName + " - conversation finished")
+
+def launch ():
+  def realStart (event=None):
+    if not core.hasComponent("messenger"):
+      if event is None:
+        # Only do this the first time
+        log.warning("Deferring firing up GuiMessengerServiceListener because Messenger isn't up yet")
+        core.addListenerByName("ComponentRegistered", realStart, once=True)
+      return
+    if not core.hasComponent(GuiMessengerServiceListener.__name__):
+      core.registerNew(GuiMessengerServiceListener)
+      log.info("Up...")
+
+  realStart()
