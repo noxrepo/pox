900c4502adf504aeaf42c1a37625bad168d2c30b
Murphy McCauley <murphy.mccauley@gmail.com>
2011-08-18 22:43:17 -0700

    dumb_l2_switch: Add docstrings

--- pox/dumb_l2_switch/__init__.py
+++ pox/dumb_l2_switch/__init__.py
@@ -1,4 +1,18 @@
+"""
+This package contains two L2 learning switches for OpenFlow.
+
+The default, dumb_l2_switch, is written directly against the OpenFlow library.
+It is derived from one written live for an SDN crash course.
+
+The other, ofcommand_l2_switch, is derived originally from NOX's pyswitch
+example.  It is now a demonstration of the ofcommand library for constructing
+OpenFlow messages.
+"""
+
 def launch ():
+  """
+  Starts an L2 learning switch.
+  """
   import dumb_l2_switch
   from pox.core import core
   core.registerNew(dumb_l2_switch.dumb_l2_switch)
--- pox/dumb_l2_switch/dumb_l2_switch.py
+++ pox/dumb_l2_switch/dumb_l2_switch.py
@@ -5,14 +5,52 @@ from pox.lib.revent.revent import *
 log = core.getLogger()
 
 class LearningSwitch (EventMixin):
+  """
+  The learning switch "brain" associated with a single OpenFlow switch.
+
+  When we see a packet, we'd like to output it on a port which will eventually
+  lead to the destination.  To accomplish this, we build a table that maps
+  addresses to ports.
+
+  We populate the table by observing traffic.  When we see a packet from some
+  source coming from some port, we know that source is out that port.
+
+  When we want to forward traffic, we look up the desintation in our table.  If
+  we don't know the port, we simply send the message out all ports except the
+  one it came in on.  (In the presence of loops, this is bad!).
+
+  In short, our algorithm looks like this:
+
+  For each new flow:
+  1) Use source address and port to update address/port table
+  2) Is destination multicast?
+     Yes:
+        2a) Flood the packet
+     No:
+        2b) Port for destination address in our address/port table?
+           No:
+             2ba) Flood the packet
+          Yes:
+             2bb1) Install flow table entry in the switch so that this flow
+                   goes out the appopriate port
+             2bb2) Send buffered packet out appopriate port
+  """
   def __init__ (self, connection):
+    # Switch we'll be adding L2 learning switch capabilities to
     self.connection = connection
+
+    # Our table
     self.macToPort = {}
 
+    # We want to hear PacketIn messages, so we listen
     self.listenTo(connection)
 
   def _handle_PacketIn (self, event):
+    """
+    Handles packet in messages from the switch to implement above algorithm.
+    """
     def flood ():
+      """ Floods the packet """
       msg = of.ofp_packet_out()
       msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
       msg.buffer_id = event.ofp.buffer_id
@@ -20,16 +58,18 @@ class LearningSwitch (EventMixin):
       self.connection.send(msg)
 
     packet = event.parse()
-    self.macToPort[packet.src] = event.port
+    self.macToPort[packet.src] = event.port # 1
     if packet.dst.isMulticast():
-      flood()
+      flood() # 2a
     else:
       if packet.dst not in self.macToPort:
         log.debug("port for %s unknown -- flooding" % (packet.dst,))
-        flood()
+        flood() # 2ba
       else:
+        # 2bb
         port = self.macToPort[packet.dst]
-        log.debug("installing flow for %s.%i -> %s.%i" % (packet.src, event.port, packet.dst, port))
+        log.debug("installing flow for %s.%i -> %s.%i" %
+                  (packet.src, event.port, packet.dst, port))
         msg = of.ofp_flow_mod()
         msg.match = of.ofp_match.from_packet(packet)
         msg.idle_timeout = 10
@@ -40,6 +80,9 @@ class LearningSwitch (EventMixin):
 
 
 class dumb_l2_switch (EventMixin):
+  """
+  Waits for OpenFlow switches to connect and makes them learning switches.
+  """
   def __init__ (self):
     self.listenTo(core.openflow)
 
