6b27d6691732895ac70c26efad8e7486f4145a4d
Murphy McCauley <murphy.mccauley@gmail.com>
2011-09-20 04:16:27 -0700

    More work towards GPL compliance

--- pox.py
+++ pox.py
@@ -1,5 +1,22 @@
 #!/bin/bash -
 
+# Copyright 2011 James McCauley
+#
+# This file is part of POX.
+#
+# POX is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# POX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with POX.  If not, see <http://www.gnu.org/licenses/>.
+
 # If you have PyPy 1.6+ in a directory called pypy alongside pox.py, we
 # use it.
 # Otherwise, we try to use a Python interpreter called python2.7, which
--- pox/misc/dnsspy.py
+++ pox/misc/dnsspy.py
@@ -1,3 +1,20 @@
+# Copyright 2011 James McCauley
+#
+# This file is part of POX.
+#
+# POX is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# POX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with POX.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 This is a port of NOX's DNSSpy component.
 """
@@ -16,7 +33,7 @@ class DNSSpy (EventMixin):
 
     self.listenTo(core.openflow)
 
-  def _handle_ConnectionUpx (self, event):
+  def _handle_ConnectionUp (self, event):
     msg = of.ofp_flow_mod()
     msg.match = of.ofp_match()
     msg.match.dl_type = ethernet.ethernet.IP_TYPE
