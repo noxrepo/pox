7a1fc1892d303f05ef120484572f8e49f350c50b
Murphy McCauley <murphy.mccauley@gmail.com>
2011-09-20 00:17:42 -0700

    openflow: Turn up listen backlog to 16
    This probably doesn't matter much in the real world, but it makes cbench
    connect much faster since all of its virtual switches try to connect at
    exactly the same time.

--- pox/openflow/of_01.py
+++ pox/openflow/of_01.py
@@ -472,7 +472,7 @@ class OpenFlow_01_Task (Task):
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listener.bind((self.address, self.port))
-    listener.listen(1)
+    listener.listen(16)
     sockets.append(listener)
     wsocks = []
 
