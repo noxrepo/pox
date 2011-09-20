d9cddffb54e5fa7d331bfeaf46a7cf2155aacb87
Murphy McCauley <murphy.mccauley@gmail.com>
2011-09-20 03:56:26 -0700

    packet: Fix GPL info
    Hopefully this fixes the GPL headers for the packet library to reflect
    that the packet library code is GPLed with two authors (me and Nicira) and
    that THIS version is part of POX.

--- pox/lib/packet/lldp.py
+++ pox/lib/packet/lldp.py
@@ -1,19 +1,24 @@
+# Copyright 2011 James McCauley
 # Copyright 2008 (C) Nicira, Inc.
 #
-# This file is part of NOX.
+# This file is part of POX.
 #
-# NOX is free software: you can redistribute it and/or modify
+# POX is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# NOX is distributed in the hope that it will be useful,
+# POX is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with NOX.  If not, see <http://www.gnu.org/licenses/>.
+# along with POX.  If not, see <http://www.gnu.org/licenses/>.
+
+# This file is derived from the packet library in NOX, which was
+# developed by Nicira, Inc.
+
 #======================================================================
 # IEEE 802.1AB Link Layer Discovery Protocol (lldp) header
 # (http://standards.ieee.org/getieee802/download/802.1AB-2005.pdf)
