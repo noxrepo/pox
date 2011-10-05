a786c612b486c7b00e689cf12018e3b545836173
Murphy McCauley <murphy.mccauley@gmail.com>
2011-10-05 01:35:40 -0700

    libopenflow: Handle signed integer IP addresses in ofp_match
    When packing ofp_match, it should now be able to handler integer IP addresses
    in both signed and unsigned format.  Of course, you really should be using
    IPAddrs anyway.

--- pox/openflow/libopenflow_01.py
+++ pox/openflow/libopenflow_01.py
@@ -559,8 +559,8 @@ class ofp_match:
     packed += '\x00\x00' # Hardcode padding
     def fix (addr):
       if addr is None: return 0
-      if type(addr) is int: return addr
-      if type(addr) is long: return addr
+      if type(addr) is int: return addr & 0xffFFffFF
+      if type(addr) is long: return addr & 0xffFFffFF
       return addr.toUnsigned()
     packed += struct.pack("!LLHH", fix(self.nw_src), fix(self.nw_dst),
                           self.tp_src or 0, self.tp_dst or 0)
