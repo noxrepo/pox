dcdd88c7f817655573732808ead7e36de6c5c4e1
Murphy McCauley <murphy.mccauley@gmail.com>
2011-10-11 04:15:42 -0700

    packet: Backing out accidental changes from r344
    r344 Was supposed to just fix a minor bug in openflow.py, but a number
    of significant changes to dhcp.py got lumped in by accident.  This
    rolls those back so that they can be properly entered.

--- pox/lib/packet/dhcp.py
+++ pox/lib/packet/dhcp.py
@@ -57,7 +57,7 @@
 #
 #======================================================================
 import struct
-from packet_utils import *
+from packet_utils       import *
 
 from packet_base import packet_base
 import pox.lib.util as util
@@ -129,50 +129,39 @@ class dhcp(packet_base):
         self.sname = b''
         self.file = b''
         self.magic = b''
-        self._raw_options = b''
+        self.options = b''
 
         if raw is not None:
             self.parse(raw)
         else:
-            self.options = util.DirtyDict()
+            self.parsedOptions = util.DirtyDict()
 
         self._init(kw)
 
     def __str__(self):
-        s  = '[op:'+str(self.op)
-        s += ' htype:'+str(self.htype)
-        s += ' hlen:'+str(self.hlen)
-        s += ' hops:'+str(self.hops)
-        s += ' xid:'+str(self.xid)
-        s += ' secs:'+str(self.secs)
-        s += ' flags:'+str(self.flags)
-        s += ' ciaddr:'+str(self.ciaddr)
-        s += ' yiaddr:'+str(self.yiaddr)
-        s += ' siaddr:'+str(self.siaddr)
-        s += ' giaddr:'+str(self.giaddr)
-        s += ' chaddr:'
-        if isinstance(self.chaddr, EthAddr):
-            s += str(self.chaddr)
-        else:
-            s += ' '.join(["{0:02x}".format(x) for x in self.chaddr])
-        s += ' magic:'+' '.join(["{0:02x}".format(x) for x in self.magic])
-        s += ' options:'+' '.join(["{0:02x}".format(x) for x in
-                                  self._raw_options])
-        s += ']'
-        return s
+        return ' '.join(('[','op:'+str(self.op),'htype:'+str(self.htype),
+                            'hlen:'+str(self.hlen),'hops:'+str(self.hops),
+                            'xid:'+str(self.xid),'secs:'+str(self.secs),
+                            'flags:'+str(self.flags),
+                            'ciaddr:'+str(self.ciaddr),
+                            'yiaddr:'+str(self.yiaddr),
+                            'siaddr:'+str(self.siaddr),
+                            'giaddr:'+str(self.giaddr),
+                            'chaddr:'+ (self.chaddr if isinstance(self.chaddr, EthAddr) else "?" ),
+                            'magic:'+str([ord(x) for x in self.magic]),
+                            'options:'+str([ord(x) for x in self.options]),']'))
 
     def parse(self, raw):
         assert isinstance(raw, bytes)
         self.raw = raw
         dlen = len(raw)
         if dlen < dhcp.MIN_LEN:
-            self.msg('(dhcp parse) warning DHCP packet data too short ' +
-                     'to parse header: data len %u' % (dlen,))
+            self.msg('(dhcp parse) warning DHCP packet data too short to parse header: data len %u' % (dlen,))
             return None
 
-        (self.op, self.htype, self.hlen, self.hops, self.xid,self.secs,
-         self.flags, self.ciaddr, self.yiaddr, self.siaddr,
-         self.giaddr) = struct.unpack('!BBBBIHHIIII', raw[:28])
+        (self.op, self.htype, self.hlen, self.hops, self.xid, self.secs,
+             self.flags, self.ciaddr, self.yiaddr, self.siaddr, self.giaddr) \
+             = struct.unpack('!BBBBIHHIIII', raw[:28])
 
         self.ciaddr = IPAddr(self.ciaddr)
         self.yiaddr = IPAddr(self.yiaddr)
@@ -181,8 +170,8 @@ class dhcp(packet_base):
 
         self.chaddr = raw[28:44]
         if self.hlen == 6:
-            # Assume chaddr is ethernet
-            self.chaddr = EthAddr(self.chaddr[:6])
+          # Assume chaddr is ethernet
+          self.chaddr = EthAddr(self.chaddr[:6])
         self.sname = raw[44:108]
         self.file = raw[102:236]
         self.magic = raw[236:240]
@@ -196,22 +185,20 @@ class dhcp(packet_base):
 
         for i in range(4):
             if dhcp.MAGIC[i] != self.magic[i]:
-                self.warn('(dhcp parse) bad DHCP magic value %s' %
-                          str(self.magic))
+                self.warn('(dhcp parse) bad DHCP magic value %s' % str(self.magic))
                 return
 
-        self._raw_options = raw[240:]
+        self.options = raw[240:]
         self.parseOptions()
         self.parsed = True
 
     def parseOptions(self):
-        self.options = util.DirtyDict()
-        self.parseOptionSegment(self._raw_options)
-        if dhcp.OVERLOAD_OPT in self.options:
-            opt_val = self.options[dhcp.OVERLOAD_OPT]
+        self.parsedOptions = util.DirtyDict()
+        self.parseOptionSegment(self.options)
+        if dhcp.OVERLOAD_OPT in self.parsedOptions:
+            opt_val = self.parsedOptions[dhcp.OVERLOAD_OPT]
             if opt_val[0] != 1:
-                self.warn('DHCP overload option has bad len %u' %
-                          (opt_val[0],))
+                self.warn('DHCP overload option has bad len %u' % (opt_val[0],))
                 return
             if opt_val[1] == 1 or opt_val[1] == 3:
                 self.parseOptionSegment(self.file)
@@ -235,11 +222,11 @@ class dhcp(packet_base):
             ofs += 1         # Account for the length octet
             if ofs + opt_len > l:
                 return False
-            if opt in self.options:
+            if opt in self.parsedOptions:
                 # Append option, per RFC 3396
-                self.options[opt] += barr[ofs:ofs+opt_len]
+                self.parsedOptions[opt] += barr[ofs:ofs+opt_len]
             else:
-                self.options[opt] = barr[ofs:ofs+opt_len]
+                self.parsedOptions[opt] = barr[ofs:ofs+opt_len]
             ofs += opt_len
         self.warn('DHCP end of option segment before END option')
 
@@ -254,50 +241,43 @@ class dhcp(packet_base):
                 o += chr(dhcp.PAD_OPT)
             return o
 
-        for k,v in self.options.iteritems():
-            if k == dhcp.END_OPT: continue
-            if k == dhcp.PAD_OPT: continue
-            if isinstance(v, bytes) and (len(v) > 255):
-                # Long option, per RFC 3396
-                v = [v[i:i+255] for i in range(0, len(v), 255)]
-            if isinstance(v, list): # Better way to tell?
-                for part in v:
-                    o += addPart(k, part)
-            else:
-                o += addPart(k, v)
+        for k,v in self.parsedOptions.iteritems():
+          if k == dhcp.END_OPT: continue
+          if k == dhcp.PAD_OPT: continue
+          if isinstance(v, bytes) and (len(v) > 255):
+              # Long option, per RFC 3396
+              v = [v[i:i+255] for i in range(0, len(v), 255)]
+          if isinstance(v, list): # Better way to tell?
+              for part in v:
+                o += addPart(k, part)
+          else:
+              o += addPart(k, v)
         o += chr(dhcp.END_OPT)
-        self._raw_options = o
-
-        if isinstance(self.options, DirtyDict):
-            self.options.dirty = False
+        self.options = o
+        self.parsedOptions.dirty = False
 
     def hdr(self, payload):
-        if isinstance(self.options, util.DirtyDict):
-            if self.options.dirty:
-                self.packOptions()
-        else:
-            self.packOptions()
+        if self.parsedOptions.dirty:
+          self.packOptions()
 
         if isinstance(self.chaddr, EthAddr):
           chaddr = self.chaddr.toRaw() + (b'\x00' * 10)
-        fmt = '!BBBBIHHIIII16s64s128s4s%us' % (len(self._raw_options),)
+        fmt = '!BBBBIHHIIII16s64s128s4s%us' % (len(self.options),)
         return struct.pack(fmt, self.op, self.htype, self.hlen,
-                           self.hops, self.xid, self.secs, self.flags,
-                           self.ciaddr, self.yiaddr, self.siaddr,
-                           self.giaddr, chaddr, self.sname, self.file,
-                           self.magic, self._raw_options)
+                    self.hops, self.xid, self.secs, self.flags,
+                    self.ciaddr, self.yiaddr, self.siaddr, self.giaddr,
+                    chaddr, self.sname,
+                    self.file, self.magic,
+                    self.options)
 
-    def appendRawOption (self, code, val = None, length = None):
-        """
-        In general, a much better way to add options should just be
-        to add them to the .options dictionary.
-        """
-        
-        self._raw_options += chr(code)
-        if length is None:
-            if val is None:
-                return
-            length = len(val)
-        self._raw_options += chr(length)
-        self._raw_options += val
+    def hasParsedOption(self, opt, len):
+        if opt not in self.parsedOptions:
+            return False
+        if len is not None and self.parsedOptions[opt][0][0] != len:
+            return False
+        return True
 
+    def addUnparsedOption(self, code, len, val):
+        self.options.append(code)
+        self.options.append(len)
+        self.options.extend(val)
