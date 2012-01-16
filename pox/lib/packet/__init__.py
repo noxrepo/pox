6df115b41d18f7e74a0220550a04459d83d391d0
Murphy McCauley <murphy.mccauley@gmail.com>
2012-01-16 02:37:49 -0800

    packet: Add all submodules to import *
    You can now access pox.lib.packet.icmp as pox.lib.packet.ICMP if
    you import the whole package (e.g., import pox.lib.packet as pkg).
    
    --HG--
    extra : rebase_source : b48d05949977468a669bdd55caab7ac898689441

--- pox/lib/packet/__init__.py
+++ pox/lib/packet/__init__.py
@@ -10,6 +10,19 @@ Could still use more work.
 
 # None of this is probably that big, and almost all of it gets loaded
 # under most circumstances anyway.  Let's just load all of it.
+import arp as ARP
+import dhcp as DHCP
+import dns as DNS
+import eap as EAP
+import eapol as EAPOL
+import ethernet as ETHERNET
+import icmp as ICMP
+import ipv4 as IPV4
+import lldp as LLDP
+import tcp as TCP
+import udp as UDP
+import vlan as VLAN
+
 from arp import *
 from dhcp import *
 from dns import *
@@ -37,4 +50,17 @@ __all__ = [
   'tcp_opt',
   'udp',
   'vlan',
+
+  'ARP',
+  'DHCP',
+  'DNS',
+  'EAP',
+  'EAPOL',
+  'ETHERNET',
+  'ICMP',
+  'IPV4',
+  'LLDP',
+  'TCP',
+  'UDP',
+  'VLAN',
 ]
