noxrepo/pox

commit 01f83795ab316ba8223676c115bcab861d35f170

switch_impl, libopenflow: implemented basic switch support for stats_req

pox/openflow/libopenflow_01.py
@@ -21,6 +21,7 @@
 
 import struct
 import operator
+import collections
 import sys
 from pox.lib.packet.packet_base import packet_base
 from pox.lib.packet.ethernet import ethernet
@@ -2120,13 +2121,32 @@ class ofp_stats_reply (ofp_header):
     self.type = 0
     self.flags = 0
     self.body = b''
+    self._body_data = (None, None)
 
     initHelper(self, kw)
 
   def _assert (self):
     return (True, None)
 
+  @property
+  def body_data (self):
+    if self._body_data[0] is not self.body:
+      def _pack(b):
+        return b.pack() if hasattr(b, 'pack') else b
+
+      data = b''
+      if isinstance(self.body, collections.Iterable):
+        for b in self.body:
+          data += _pack(b)
+      else:
+        data = _pack(self.body)
+      self._body_data = (self.body, data)
+    return self._body_data[1]
+
   def pack (self, assertstruct=True):
+    if type == None or type == 0 and type(self.body) in ofp_stats_reply_class_to_type_map:
+      self.type = ofp_stats_reply_class_to_type_map[type(self.body)]
+
     self.length = len(self)
     if(assertstruct):
       if(not self._assert()[0]):
@@ -2134,7 +2154,7 @@ class ofp_stats_reply (ofp_header):
     packed = ""
     packed += ofp_header.pack(self)
     packed += struct.pack("!HH", self.type, self.flags)
-    packed += self.body
+    packed += self.body_data
     return packed
 
   def unpack (self, binaryString):
@@ -2449,7 +2469,7 @@ class ofp_aggregate_stats_request (object):
     outstr += prefix + 'out_port: ' + str(self.out_port) + '\n'
     return outstr
 
-class ofp_aggregate_stats_reply (object):
+class ofp_aggregate_stats (object):
   def __init__ (self, **kw):
     self.packet_count = 0
     self.byte_count = 0
@@ -2657,6 +2677,23 @@ class ofp_port_stats (object):
 
   def __ne__ (self, other): return not self.__eq__(other)
 
+  def __add__(self, other):
+    if type(self) != type(other): raise NotImplemented()
+    return ofp_port_stats(
+        port_no=OFPP_NONE,
+        rx_packets = self.rx_packets + other.rx_packets,
+        tx_packets = self.tx_packets + other.tx_packets,
+        rx_bytes = self.rx_bytes + other.rx_bytes,
+        tx_bytes = self.tx_bytes + other.tx_bytes,
+        rx_dropped = self.rx_dropped + other.rx_dropped,
+        tx_dropped = self.tx_dropped + other.tx_dropped,
+        rx_errors = self.rx_errors + other.rx_errors,
+        tx_errors = self.tx_errors + other.tx_errors,
+        rx_frame_err = self.rx_frame_err + other.rx_frame_err,
+        rx_over_err = self.rx_over_err + other.rx_over_err,
+        rx_crc_err = self.rx_crc_err + other.rx_crc_err,
+        collisions = self.collisions + other.collisions)
+
   def show (self, prefix=''):
     outstr = ''
     outstr += prefix + 'port_no: ' + str(self.port_no) + '\n'
@@ -2770,6 +2807,16 @@ class ofp_queue_stats (object):
     outstr += prefix + 'tx_packets: ' + str(self.tx_packets) + '\n'
     outstr += prefix + 'tx_errors: ' + str(self.tx_errors) + '\n'
     return outstr
+
+ofp_stats_reply_class_to_type_map = {
+    ofp_desc_stats : ofp_stats_types_rev_map['OFPST_DESC'],
+    ofp_flow_stats : ofp_stats_types_rev_map['OFPST_FLOW'],
+    ofp_aggregate_stats : ofp_stats_types_rev_map['OFPST_AGGREGATE'],
+    ofp_table_stats : ofp_stats_types_rev_map['OFPST_TABLE'],
+    ofp_port_stats : ofp_stats_types_rev_map['OFPST_PORT'],
+    ofp_queue_stats : ofp_stats_types_rev_map['OFPST_QUEUE']
+}
+
 
 ##3.6 Send Packet Message
 class ofp_packet_out (ofp_header):

pox/lib/util.py
@@ -390,6 +390,7 @@ def connect_socket_with_backoff(address, port, max_backoff_seconds=32):
   '''
   backoff_seconds = 1
   sock = None
+  print >>sys.stderr, "connect_socket_with_backoff(address=%s, port=%d)" % (address, port)
   while True:
     try:
       sock = socket.socket()

pox/openflow/switch_impl.py
@@ -69,8 +69,11 @@ class SwitchImpl(EventMixin):
 
     ## Hash of port_no -> openflow.pylibopenflow_01.ofp_phy_ports
     self.ports = {}
+    self.port_stats = {}
     for port in ports:
       self.ports[port.port_no] = port
+      self.port_stats[port.port_no] = ofp_port_stats(port_no=port.port_no)
+
     ## (OpenFlow Handler map)
     self.ofp_handlers = {
        # Reactive handlers
@@ -80,7 +83,9 @@ class SwitchImpl(EventMixin):
        ofp_type_rev_map['OFPT_FLOW_MOD'] : self._receive_flow_mod,
        ofp_type_rev_map['OFPT_PACKET_OUT'] : self._receive_packet_out,
        ofp_type_rev_map['OFPT_BARRIER_REQUEST'] : self._receive_barrier_request,
+       ofp_type_rev_map['OFPT_GET_CONFIG_REQUEST'] : self._receive_get_config_request,
        ofp_type_rev_map['OFPT_SET_CONFIG'] : self._receive_set_config,
+       ofp_type_rev_map['OFPT_STATS_REQUEST'] : self._receive_stats_request,
        ofp_type_rev_map['OFPT_VENDOR'] : self._receive_vendor,
        # Proactive responses
        ofp_type_rev_map['OFPT_ECHO_REPLY'] : self._receive_echo_reply
@@ -166,9 +171,68 @@ class SwitchImpl(EventMixin):
     msg = ofp_barrier_reply(xid = ofp.xid)
     self.send(msg)
 
+  def _receive_get_config_request(self, ofp):
+    self.log.debug("Get config request %s %s " % (self.name, str(ofp)))
+    msg = ofp_get_config_reply(xid = ofp.xid)
+    self.send(msg)
+
+  def _receive_stats_request(self, ofp):
+    self.log.debug("Get stats request %s %s " % (self.name, str(ofp)))
+ 
+    def desc_stats(ofp):
+      return ofp_desc_stats(mfr_desc="BadAssEmulatedPoxSwitch(TM)",
+                            hw_desc="your finest emulated asics",
+                            sw_desc="pox. reliable, fast, stable. Choose 0 (or more?)",
+                            serial_num=str(self.dpid),
+                            dp_desc="high performance emuswitch. Several packets per second have been observed (but not by reliable witnesses)")
+
+    def flow_stats(ofp):
+      req = ofp_flow_stats_request().unpack(ofp.body)
+      assert(self.table_id == TABLE_ALL)
+      return self.table.flow_stats(req.match, req.out_port)
+
+    def aggregate_stats(ofp):
+      req = ofp_aggregate_stats_request().unpack(ofp.body)
+      assert(self.table_id == TABLE_ALL)
+      return self.table.aggregate_stats(req.match, out_port)
+
+    def table_stats(ofp):
+      return self.table.table_stats()
+
+    def port_stats(ofp):
+      req = ofp_port_stats_request().unpack(ofp.body)
+      if req.port_no == OFPP_NONE:
+        res = ofp_port_stats(port_no=OFPP_NONE)
+        for stats in self.port_stats.values():
+          res += stats
+        return res
+      else:
+        return self.port_stats[req.port_no]
+
+    def queue_stats(ofp):
+      raise AttributeError("not implemented")
+
+    stats_handlers = {
+        OFPST_DESC: desc_stats,
+        OFPST_FLOW: flow_stats,
+        OFPST_AGGREGATE: aggregate_stats,
+        OFPST_TABLE: table_stats,
+        OFPST_PORT: port_stats,
+        OFPST_QUEUE: queue_stats
+    }
+
+    if ofp.type in stats_handlers:
+      handler = stats_handlers[ofp.type]
+    else:
+      raise AttributeError("Unsupported stats request type %d" % ofp.type)
+
+    reply = ofp_stats_reply(xid=ofp.xid, body=handler(ofp))
+    self.log.debug("Sending stats reply %s %s" % (self.name, str(reply)))
+    self.send(reply)
+
   def _receive_set_config(self, config):
-    self.log.debug("Set  config %s %s" % (self.name, str(config)))
-    
+    self.log.debug("Set config %s %s" % (self.name, str(config)))
+
   def _receive_vendor(self, vendor):
     self.log.debug("Vendor %s %s" % (self.name, str(vendor)))
     # We don't support vendor extensions, so send an OFP_ERROR, per page 42 of spec
@@ -505,7 +569,7 @@ class ControllerConnection (object):
 
       try:
         if ofp_type not in self.ofp_handlers:
-          raise RuntimeError("No handler for ofp_type %d" % ofp_type)
+          raise RuntimeError("No handler for ofp_type %s(%d)" % (ofp_type_map.get(ofp_type), ofp_type))
 
         h = self.ofp_handlers[ofp_type]
         h(msg_obj)

commit ce7d5e1785c8476ea965bb013f43bc78a9075812

libopenflow: Fix two minor stats-related bugs
This closes Issue #16.

One of these is a regression from 01f83795a, which changed the name
of ofp_aggregate_stats_reply to ofp_aggregate_stats.  The latter is
how it appears in the spec, but we explicitly decided to disregard
that.  Both names now work.  (But use the _reply one.)

The other seems to just have been a bug.

pox/openflow/libopenflow_01.py
@@ -2366,7 +2366,7 @@ class ofp_flow_stats (object):
     return packed
 
   def unpack (self, binaryString):
-    if (len(binaryString) < 48 + len(self.match())):
+    if (len(binaryString) < 48 + len(self.match)):
       return binaryString
     (self.length, self.table_id, pad) = struct.unpack_from("!HBB", binaryString, 0)
     self.match.unpack(binaryString[4:])
@@ -2513,6 +2513,7 @@ class ofp_aggregate_stats (object):
     outstr += prefix + 'byte_count: ' + str(self.byte_count) + '\n'
     outstr += prefix + 'flow_count: ' + str(self.flow_count) + '\n'
     return outstr
+ofp_aggregate_stats_reply = ofp_aggregate_stats
 
 class ofp_table_stats (object):
   def __init__ (self, **kw):