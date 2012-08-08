6890947b4f792bae7f014b1a1f936620089f2231
Murphy McCauley <murphy.mccauley@gmail.com>
2012-08-08 13:57:33 -0700

    pretty_log: Tweak subsystem name field

--- pox/samples/pretty_log.py
+++ pox/samples/pretty_log.py
@@ -27,5 +27,5 @@ def launch ():
   import pox.log.color
   pox.log.color.launch()
   import pox.log
-  pox.log.launch(format="[@@@bold@@@level%(name)-22s@@@reset] " +
+  pox.log.launch(format="[@@@bold@@@level%(name)-23s@@@reset] " +
                         "@@@bold%(message)s@@@normal")
