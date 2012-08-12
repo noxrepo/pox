238efd8420eed90056e6e1af1fd032216701a099
Murphy McCauley <murphy.mccauley@gmail.com>
2012-08-11 17:21:54 -0700

    messenger/web: Update webmessenger demo stuff

--- pox/web/www_root/webmessenger.js
+++ pox/web/www_root/webmessenger.js
@@ -1,5 +1,5 @@
 /*************************************************************************
-Copyright 2011 James McCauley
+Copyright 2011,2012 James McCauley
 
 This file is part of POX.
 
@@ -17,47 +17,163 @@ You should have received a copy of the GNU General Public License
 along with POX.  If not, see <http://www.gnu.org/licenses/>.
 **************************************************************************/
 
-// This class communicates with POX's webmessenger component.
-// It could be improved (especially with some tweaks to
-// webmessenger.  Feel free to contribute improvements. :)
-// (In particular, it currently *detects* problems, but does
-// not recover from them.)
-function WebMessenger (callback, urlBase, user, password)
+/*
+This is a little API for communicating with POX's webmessenger
+component.  It's pretty awful.  One can do MUCH better with
+some better JS, especially with a class framework like qooxdoo.
+
+It also could use some improvements (and some corresponding
+improvements to the webmessenger).  In particular, it currently
+*detects* various problems, but provides no way to recover from
+them.
+
+Please feel free to contribute improvements!
+*/
+
+function MessengerChannel (messenger, channelName, config)
 {
+  this._channelName = channelName;
+  this._messenger = messenger;
+  this.on_rx = function (data)
+  {
+    if (console) console.log("Channel " + this._channelName
+                             + "rx: " + JSON.stringify(data));
+  };
+  this.on_disconnect = function () {};
+
+  if (config)
+  {
+    if (config.on_rx) this.on_rx = config.on_rx;
+    if (config.on_disconnect) this.on_disconnect = config.on_disconnect;
+  }
+
+  this.send = function (msg)
+  {
+    // Should probably copy this before sending, but whatever.
+    msg['CHANNEL'] = this._channelName;
+    this._messenger.send();
+  };
+}
+
+function WebMessenger (url, user, password, on_connect, on_disconnect, on_rx)
+{
+
+  this._reset = function ()
+  {
+    this._ses = null;
+    this._rx_seq = null;
+    this._tx_seq = 100;
+    this._stopped = false;
+  };
+
+  var autorestart;
+  if (typeof(url) != 'string' && typeof(url) != 'undefined')
+  {
+    user = url.user || user;
+    password = url.password || password;
+    on_connect = url.on_connect || on_connect;
+    on_disconnect = url.on_disconnect || on_disconnect;
+    on_rx = url.on_rx || on_rx;
+    autorestart = url.autorestart;
+    url = url.url;
+  }
+
+  this._reset();
+
+  this._channels = {};
+
+  /*
   this._ses = null;
   this._rx_seq = null;
   this._tx_seq = 100;
   this._stopped = false;
+  */
 
-  if (!urlBase) urlBase = "/_webmsg";
-  if (urlBase[urlBase.length-1] != "/") urlBase += "/";
-  this._urlBase = urlBase
+  if (!url) url = "/_webmsg";
+  if (url[url.length-1] != "/") url += "/";
+  this._urlBase = url;
   this._auth = [user, password];
 
   this._output = []; // Queue of outgoing data
   this._output_pending = false;
   this._poll_pending = false;
 
-  this.stop = function (data)
+  this.autorestart = !!autorestart;
+  this._restart_pending = false;
+
+  this.stop = function (can_restart)
   {
     this._stopped = true;
     if (this._timer) clearInterval(this._timer);
     this._timer = null;
     //TODO: Abort pending connections
+
+    if (can_restart && this.autorestart && !this._restart_pending)
+    {
+      this._restart_pending = true;
+      var self = this;
+      setTimeout(function () { self.connect(); }, 5000);
+      //TODO: use the timer as _restart_pending (and cancel later)
+    }
   };
 
-  if (callback)
+  this.get_channel = function (channel, extra, channel_config)
   {
-    this.process = callback
-  }
-  else
-  {
-    this.process = function (data)
+    if (channel in this._channels)
     {
-      // You probably want to overload this...
-      if (console) console.log("Recv: " + JSON.stringify(data));
-    };
-  }
+      if (console) console.log("Ignored extra join info");
+      return this._channels[channel];
+    }
+
+    var join = {'CHANNEL':'','cmd':'join_channel','channel':channel};
+    for (var x in extra) join[x] = extra[x];
+    this.send(join);
+    var chan = new MessengerChannel(this, channel, channel_config);
+    this._channels[channel] = chan;
+    return chan;
+  };
+
+  this.process = function (data)
+  {
+    // You probably want to overload this...
+    if ('CHANNEL' in data && data.CHANNEL in this._channels)
+    {
+      this._channels[data.CHANNEL].on_rx(data);
+    }
+    else
+    {
+      this.on_rx(data);
+    }
+  };
+
+  this.on_connect = function (session_id)
+  {
+    if (console) console.log("Connect: Session " + session_id);
+  };
+  if (on_connect) this.on_connect = on_connect;
+  this.on_disconnect = function (msg, stat)
+  {
+    if (!this.autorestart) alert(msg);
+  };
+  if (on_disconnect) this.on_disconnect = on_disconnect;
+  this.on_rx = function (data)
+  {
+    if (console) console.log("on_rx: " + JSON.stringify(data));
+  };
+  if (on_rx) this.on_rx = on_rx;
+
+  this._do_disconnect = function (msg, stat)
+  {
+    if (console) console.log("Disconnect: " + msg);
+    for (var c in this._channels)
+    {
+      c = this._channels[c];
+      c.on_disconnect(msg, stat);
+      //try { c.on_disconnect(msg); } catch (e) { };
+    }
+    //try { this.on_disconnect(msg); } catch (e) { };
+    this.on_disconnect(msg, stat);
+  };
 
   this._nextTXSeq = function ()
   {
@@ -75,23 +191,28 @@ function WebMessenger (callback, urlBase, user, password)
     if (req.readyState !== 4) return;
     if (req.status != 200)
     {
-      if (req.status !== 0)
+      //if (req.status !== 0)
       {
-        alert("Communication error (status " + req.status + ")");
+        this._do_disconnect("Communication error (status " + req.status + ")", req.status);
       }
-      this.stop();
+      this.stop(true);
       return;
     }
     var sendPoll = req.isPoll || (this._ses === null);
 
     var data = JSON.parse(req.responseText);
-    if (this._ses === null && console) console.log("Session: " + data.ses);
+    if (this._ses === null && console)
+    {
+      //console.log("Session: " + data.ses);
+      this._restart_pending = false;
+      this.on_connect(data.ses);
+    }
     if (this._ses === null) this._ses = data.ses;
     if (data.ses !== this._ses)
     {
       // Strange!
-      this.stop();
-      alert("Communication error (bad ses)");
+      this.stop(true);
+      this._do_disconnect("Communication error (bad ses)", -1);
       return;
     }
     if (req.isPoll)
@@ -101,8 +222,8 @@ function WebMessenger (callback, urlBase, user, password)
       {
         //alert(data.seq + " " + this._rx_seq);
         // Might want to buffer, but for now...
-        this.stop();
-        alert("Communication error (bad seq)");
+        this.stop(true);
+        this._do_disconnect("Communication error (bad seq)", -2);
         return;
       }
       if (this._rx_seq === 0x7FffFFff)
@@ -220,8 +341,12 @@ function WebMessenger (callback, urlBase, user, password)
   {
     // Kick it off by sending a dummy message.
     // The response is where we get our session key.
+    if (console) console.log("Connecting...");
+    this._reset();
     this._sendKeepAlive();
+    this._restart_pending = false;
     var self = this;
-    this._timer = setInterval(function () { self._handleKeepAliveTimer(); }, 60*1000);
+    this._timer = setInterval(function (){ self._handleKeepAliveTimer(); },
+                              60*1000);
   };
 }
