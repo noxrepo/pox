d14223e2b147cd5576fe86b417c39d4533882f78
Murphy McCauley <murphy.mccauley@gmail.com>
2012-09-20 18:34:53 -0700

    core: Don't create logger names with "ext."
    Things in ext shouldn't have to have "ext" prefixed on their name.
    We should probably check if they actually have it because they're in
    the ext directory, but whatever.

--- pox/core.py
+++ pox/core.py
@@ -67,6 +67,9 @@ def getLogger (name=None, moreFrames=0):
           del n[-1]
           name = '.'.join(n)
 
+    if name.startswith("ext."):
+      name = name.split("ext.",1)[1]
+
     if name.endswith(".__init__"):
       name = name.rsplit(".__init__",1)[0]
 
