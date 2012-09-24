d0e459eac90e6ba09be7b84fa4b9c0fa7b0302f6
Murphy McCauley <murphy.mccauley@gmail.com>
2012-09-24 03:43:19 -0700

    forwarding: Add L2 pairs learning switch
    This is a *really* simple demo that installs rules
    for each src/dst L2 pair, much like POXDesk's L2LS.

---
+++ pox/forwarding/l2_pairs.py
@@ -0,0 +1,81 @@
+# Copyright 2012 James McCauley
+#
+# This file is part of POX.
+#
+# POX is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# POX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with POX.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+A super simple OpenFlow learning switch that installs rules for
+each pair of L2 addresses.
+"""
+
+# These next two imports are common POX convention
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+
+
+# Even a simple usage of the logger is much nicer than print!
+log = core.getLogger()
+
+
+# This table maps (switch,MAC-addr) pairs to the port on 'switch' at
+# which we last saw a packet *from* 'MAC-addr'.
+# (In this case, we use a Connection object for the switch.)
+table = {}
+
+
+# Handle messages the switch has sent us because it has no
+# matching rule.
+def _handle_PacketIn (event):
+  packet = event.parsed
+
+  # Learn the source
+  table[(event.connection,packet.src)] = event.port
+
+  dst_port = table.get((event.connection,packet.dst))
+
+  if dst_port is None:
+    # We don't know where the destination is yet.  So, we'll just
+    # send the packet out all ports (except the one it came in on!)
+    # and hope the destination is out there somewhere. :)
+    # To send out all ports, we can use either of the special ports
+    # OFPP_FLOOD or OFPP_ALL.  We'd like to just use OFPP_FLOOD,
+    # but it's not clear if all switches support this. :(
+    msg = of.ofp_packet_out(resend = event.ofp)
+    msg.actions.append(of.ofp_action_output(port = of.OFPP_ALL))
+    msg.send(event.connection)
+  else:
+    # Since we know the switch ports for both the source and dest
+    # MACs, we can install rules for both directions.
+    msg = of.ofp_flow_mod()
+    msg.match.dl_dst = packet.src
+    msg.match.dl_src = packet.dst
+    msg.actions.append(of.ofp_action_output(port = event.port))
+    msg.send(event.connection)
+    
+    # This is the packet that just came in -- we want to
+    # install the rule and also resend the packet.
+    msg = of.ofp_flow_mod()
+    msg.match.dl_src = packet.src
+    msg.match.dl_dst = packet.dst
+    msg.actions.append(of.ofp_action_output(port = dst_port))
+    msg.send(event.connection, resend = event.ofp)
+
+    log.debug("Installing %s <-> %s" % (packet.src, packet.dst))
+
+
+def launch ():
+  core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
+
+  log.info("Pair-Learning switch running.")
