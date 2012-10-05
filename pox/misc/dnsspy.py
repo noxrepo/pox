2c02fbb3a7909c232200c1651148573c7b1138e2
Murphy McCauley <murphy.mccauley@gmail.com>
2012-10-04 22:31:34 -0700

    misc: Improve/modernize dnsspy component

--- pox/misc/dnsspy.py
+++ pox/misc/dnsspy.py
@@ -1,4 +1,4 @@
-# Copyright 2011 James McCauley
+# Copyright 2011-2012 James McCauley
 # Copyright 2008 (C) Nicira, Inc.
 #
 # This file is part of POX.
@@ -17,50 +17,75 @@
 # along with POX.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-This is a port of NOX's DNSSpy component.
+This component spies on DNS replies.
+
+Similar to NOX's DNSSpy component.
 """
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
-from pox.lib.revent import *
-from pox.lib.packet import *
+import pox.lib.packet as pkt
+from pox.lib.addresses import IPAddr
 
 log = core.getLogger()
 
-class DNSSpy (EventMixin):
+class DNSSpy (object):
   def __init__ (self):
-    self.ip_records = {}
+    self.ip_to_name = {}
+    self.name_to_ip = {}
 
-    self.listenTo(core.openflow)
+    core.openflow.addListeners(self)
+
+    # Add handy function to console
+    core.Interactive.variables['lookup'] = self.lookup
 
   def _handle_ConnectionUp (self, event):
     msg = of.ofp_flow_mod()
     msg.match = of.ofp_match()
-    msg.match.dl_type = ethernet.IP_TYPE
-    msg.match.nw_proto = ipv4.UDP_PROTOCOL
+    msg.match.dl_type = pkt.ethernet.IP_TYPE
+    msg.match.nw_proto = pkt.ipv4.UDP_PROTOCOL
     msg.match.tp_src = 53
     msg.actions.append(of.ofp_action_output(port = of.OFPP_CONTROLLER))
     event.connection.send(msg)
 
+  def lookup (self, something):
+    if something in self.name_to_ip:
+      return self.name_to_ip[something]
+    try:
+      return self.ip_to_name.get(IPAddr(something))
+    except:
+      return None
+
+  def _record (self, ip, name):
+    modified = False
+    val = self.ip_to_name.setdefault(ip, [])
+    if name not in val:
+      val.insert(0, name)
+      modified = True
+
+    val = self.name_to_ip.setdefault(name, [])
+    if ip not in val:
+      val.insert(0, ip)
+      modified = True
+
+    return modified
+
   def _handle_PacketIn (self, event):
-    p = event.parse().find('dns')
-    if p is not None:
+    p = event.parsed.find('dns')
+
+    if p is not None and p.parsed:
       log.debug(p)
 
       for answer in p.answers:
-        if answer.qtype == dns.dns.rr.A_TYPE:
-          val = self.ip_records[answer.rddata]
-          if answer.name not in val:
-            val.insert(0, answer.name)
-
+        if answer.qtype == pkt.dns.rr.A_TYPE:
+          if self._record(answer.rddata, answer.name):
             log.info("add dns entry: %s %s" % (answer.rddata, answer.name))
 
       for addition in p.additional:
-        if addition.qtype == dns.dns.rr.A_TYPE: 
-          val = self.ip_records[addition.rddata]
-          if addition.name not in val:
-            val.insert(0, addition.name)
-            log.info("additional dns entry: %s %s" % (addition.rddata, addition.name))
+        if addition.qtype == pkt.dns.rr.A_TYPE: 
+          if self._record(addition.rddata, addition.name):
+            log.info("additional dns entry: %s %s" % (addition.rddata,
+                                                      addition.name))
 
 
 def launch ():
