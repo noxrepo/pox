5f3398f15a402f8943abeacd345a8def3e0774bc
Murphy McCauley <murphy.mccauley@gmail.com>
2013-02-23 04:57:22 -0800

    datapaths.nx_switch: Cleanup
    This mostly just improves POX coding convention adherence.

--- pox/datapaths/nx_switch.py
+++ pox/datapaths/nx_switch.py
@@ -1,4 +1,4 @@
-# Copyright 2011 Andreas Wundsam
+# Copyright 2011,2012 Andreas Wundsam
 #
 # This file is part of POX.
 #
@@ -21,27 +21,38 @@ import pox.openflow.libopenflow_01 as of
 import pox.openflow.nicira_ext as nx
 from pox.datapaths.switch import SoftwareSwitch, OFConnection
 
-_slave_blacklist = set([of.ofp_flow_mod, of.ofp_packet_out, of.ofp_port_mod, of.ofp_barrier_request])
+_slave_blacklist = set([of.ofp_flow_mod, of.ofp_packet_out, of.ofp_port_mod,
+                        of.ofp_barrier_request])
 _messages_for_all = set([of.ofp_port_status])
 
-class NXSoftwareSwitch(SoftwareSwitch):
-  """ Extension of the SwichImpl software switch that supports the nicira (NX) vendor extension from
-      OpenVSwitch that allows the switch to connect to multiple controllers at the same time.
 
-      In the beginning, all controllers start out as equals (ROLE_OTHER). Through the NX vendor message
-      role_request, one controller can be promoted to ROLE_MASTER, in which case all other controllers
-      are downgraded to slave status.
-
-      The switch doesn't accept state-mutating messages (e.g., FLOW_MOD, see _slave_blacklist) from
-      slave controllers.
-
-      Messages are distributed to controllers according to their type:
-       - symmetric message replies are sent to the controller that initiated them (e.g., STATS_REQUEST -> REPLY)
-       - port_status messages are distributed to all controllers
-       - all other messages are distributed to the master controller, or if non is present, any controller
-         in ROLE_OTHER
+class NXSoftwareSwitch (SoftwareSwitch):
   """
-  def __init__(self, *args, **kw):
+  Software datapath with Nicira (NX) extensions
+
+  Extension of the software switch that supports some of the Nicira (NX) vendor
+  extensions that are part of Open vSwitch.
+  
+  In particular, this include the ability for a switch to connect to multiple
+  controllers at the same time.
+
+  In the beginning, all controllers start out as equals (ROLE_OTHER). Through
+  the NX vendor message role_request, one controller can be promoted to
+  ROLE_MASTER, in which case all other controllers are downgraded to slave
+  status.
+
+  The switch doesn't accept state-mutating messages (e.g., FLOW_MOD, see
+  _slave_blacklist) from slave controllers.
+
+  Messages are distributed to controllers according to their type:
+    - symmetric message replies are sent to the controller that initiated them
+      (e.g., STATS_REQUEST -> REPLY)
+    - port_status messages are distributed to all controllers
+    - all other messages are distributed to the master controller, or if none
+      is present, any controller in ROLE_OTHER
+  """
+
+  def __init__ (self, *args, **kw):
     SoftwareSwitch.__init__(self, *args, **kw)
     self.role_by_conn={}
     self.connections = []
@@ -50,83 +61,93 @@ class NXSoftwareSwitch(SoftwareSwitch):
     # (for round robin of async messages)
     self.next_other = 0
 
-  def on_message_received(self, connection, msg):
-    """ @overrides SoftwareSwitch.on_message_received"""
+  def on_message_received (self, connection, msg):
+    """
+    Handles incoming messages
+    
+    @overrides SoftwareSwitch.on_message_received
+    """
 
     self.connection_in_action = connection
     if not self.check_rights(msg, connection):
-      self.log.warn("Message %s not allowed for slave controller %d", msg, connection.ID)
-      self.send_error(connection)
+      self.log.warn("Message %s not allowed for slave controller %d", msg,
+                    connection.ID)
+      self.send_vendor_error(connection)
     else:
       SoftwareSwitch.on_message_received(self, connection, msg)
 
     self.connection_in_action = None
 
-  def check_rights(self, ofp, connection):
+  def check_rights (self, ofp, connection):
     if self.role_by_conn[connection.ID] != nx.ROLE_SLAVE:
       return True
     else:
       return not type(ofp) in _slave_blacklist
 
-  def send_error(self, connection):
-    # We don't support vendor extensions, so send an OFP_ERROR, per page 42 of spec
+  def send_vendor_error (self, connection):
     err = of.ofp_error(type=of.OFPET_BAD_REQUEST, code=of.OFPBRC_BAD_VENDOR)
     connection.send(err)
 
-  def send(self, message):
+  def send (self, message):
     connections_used = []
     if type(message) in _messages_for_all:
       for c in self.connections:
         c.send(message)
         connections_used.append(c)
     elif self.connection_in_action:
-      #self.log.info("Sending %s to active connection %d", (str(message), self.connection_in_action.ID))
+      #self.log.info("Sending %s to active connection %d",
+      #              (str(message), self.connection_in_action.ID))
       self.connection_in_action.send(message)
       connections_used.append(self.connection_in_action)
     else:
-      masters = [c for c in self.connections if self.role_by_conn[c.ID] == nx.ROLE_MASTER]
+      masters = [c for c in self.connections
+                 if self.role_by_conn[c.ID] == nx.ROLE_MASTER]
       if len(masters) > 0:
         masters[0].send(message)
         connections_used.append(masters[0])
       else:
-        others = [c for c in self.connections if self.role_by_conn[c.ID] == nx.ROLE_OTHER]
+        others = [c for c in self.connections
+                  if self.role_by_conn[c.ID] == nx.ROLE_OTHER]
         if len(others) > 0:
           self.next_other = self.next_other % len(others)
-          #self.log.info("Sending %s to 'other' connection %d", (str(message), self.next_other))
+          #self.log.info("Sending %s to 'other' connection %d",
+          #              (str(message), self.next_other))
           others[self.next_other].send(message)
           connections_used.append(others[self.next_other])
           self.next_other += 1
         else:
-          self.log.info("Could not find any connection to send messages %s", str(message))
+          self.log.info("Could not find any connection to send messages %s",
+                        str(message))
     return connections_used
 
-  def add_connection(self, connection):
+  def add_connection (self, connection):
     self.role_by_conn[connection.ID] = nx.ROLE_OTHER
     connection.set_message_handler(self.on_message_received)
     self.connections.append(connection)
     return connection
 
-  def set_connection(self, connection):
+  def set_connection (self, connection):
     self.add_connection(connection)
 
-  def set_role(self, connection, role):
+  def set_role (self, connection, role):
     self.role_by_conn[connection.ID] = role
     if role == nx.ROLE_MASTER:
       for c in self.connections:
         if c != connection:
           self.role_by_conn[c.ID] = nx.ROLE_SLAVE
 
-  def _receive_vendor(self, vendor, connection):
+  def _receive_vendor (self, vendor, connection):
     self.log.debug("Vendor %s %s", self.name, str(vendor))
     if(vendor.vendor == nx.VENDOR_ID):
       try:
         data = nx.unpack_vendor_data_nx(vendor.data)
         if isinstance(data, nx.role_request_data):
           self.set_role(connection, data.role)
-          reply = of.ofp_vendor(xid=vendor.xid, vendor = nx.VENDOR_ID, data = nx.role_reply_data(role = data.role))
+          reply = of.ofp_vendor(xid=vendor.xid, vendor = nx.VENDOR_ID,
+                                data = nx.role_reply_data(role = data.role))
           self.send(reply)
           return
       except NotImplementedError:
-        self.send_error(connection)
+        self.send_vendor_error(connection)
     else:
       return SoftwareSwitch._receive_vendor(self, vendor)
