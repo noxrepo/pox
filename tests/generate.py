830885f00098b0876a146cc4ea004004d02d7183
Murphy McCauley <murphy.mccauley@gmail.com>
2013-05-31 13:05:01 -0700

    tests: Clean up/improve test generator

--- tests/generate.py
+++ tests/generate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
 # Copyright 2011-2012 Andreas Wundsam
+# Copyright 2013 James McCauley
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +14,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""
+Generates skeleton unit tests for POX modules
+"""
 
 from fnmatch import fnmatchcase
 import hashlib
@@ -28,10 +33,15 @@ SCRIPT_DIR=path.dirname(path.abspath(sys.modules['__main__'].__file__))
 ROOT=path.abspath(path.join(SCRIPT_DIR,".."))
 UNIT_TEST=path.join(ROOT, "tests/unit")
 
-parser = OptionParser(usage="usage: %prog [--force] <module_glob> [<module_glob>...]",
+parser = OptionParser(
+    usage="usage: %prog [--force] <module_glob> [<module_glob>...]",
     description="Generates skeleton unit tests for pox modules",
-    epilog="Arguments:\nmodule_glob: fully qualified python module name, e.g., pox.openflow.topology. Supports shell-type globs, e.g., pox.openflow.*\n")
-parser.add_option("-f", "--force", help="force overwriting existing unit tests, even when no valid autogeneration signature is found", action="store_true", dest="force", default=False)
+    epilog="Arguments: module_glob: fully qualified python module name, "
+           "e.g., pox.openflow.topology. Supports shell-type globs, e.g., "
+           "pox.openflow.*")
+parser.add_option("-f", "--force", help="force overwriting existing unit "
+    "tests, even when no valid autogeneration signature is found",
+    action="store_true", dest="force", default=False)
 
 (options, args) = parser.parse_args()
 
@@ -62,16 +72,31 @@ def mkdir(d):
 template = Template(
 """#!/usr/bin/env python
 
+# Copyright 20xx Some Developer
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import itertools
 import os.path
 import sys
 import unittest
 
-sys.path.append(os.path.join(os.path.dirname(__file__), *itertools.repeat("..", ${path_depth})))
+sys.path.append(os.path.join(os.path.dirname(__file__),
+    *itertools.repeat("..", ${path_depth})))
 
 from ${module} import *
 
-class ${test_class_name}(unittest.TestCase):
+class ${test_class_name} (unittest.TestCase):
   pass
 
 """)
@@ -101,13 +126,15 @@ def generate_test(module):
     return s.hexdigest()
 
   sha1sum = sha1(test)
-  hashed_test = re.sub(r'\n', "\n### auto generate sha1: "+sha1sum + "\n", test, 1)
+  hashed_test = re.sub(r'\n', "\n### auto generate sha1: "
+      + sha1sum + "\n", test, 1)
 
   def write_test(update=False):
     with open(test_file, "w") as f:
       f.write(hashed_test)
       f.close()
-    os.chmod(test_file, stat.S_IWUSR|stat.S_IRUSR|stat.S_IXUSR|stat.S_IXGRP|stat.S_IRGRP|stat.S_IXOTH|stat.S_IROTH)
+    os.chmod(test_file, stat.S_IWUSR|stat.S_IRUSR|stat.S_IXUSR
+        |stat.S_IXGRP|stat.S_IRGRP|stat.S_IXOTH|stat.S_IROTH)
 
   if not os.path.exists(test_file) or options.force:
     print "Creating test %s in %s" % (test_class_name, test_file)
@@ -126,12 +153,16 @@ def generate_test(module):
         print "Updating test %s in %s" % (test_class_name, test_file)
         write_test(True)
       else:
-        print "Test for %s in %s already exists (and sha1 sums don't match: %s<=>%s)" % (test_class_name, test_file, read_sha1, calculated_sha1)
+        print ("Test for %s in %s already exists (and sha1 sums don't "
+               "match: %s<=>%s)") % (test_class_name,
+               test_file, read_sha1, calculated_sha1)
     else:
-      print "Test for %s in %s already exists (and not autogeneration sig found)" % (test_class_name, test_file)
+      print ("Test for %s in %s already exists (and no autogeneration "
+             "sig found)") % (test_class_name, test_file)
 
+count = 0
 for module in modules:
-  if any(map(lambda(glob): fnmatchcase(module, glob), args)):
+  if any(fnmatchcase(module,glob) for glob in args):
+    count += 1
     generate_test(module)
-
-
+print "Created/updated",count,"tests"
