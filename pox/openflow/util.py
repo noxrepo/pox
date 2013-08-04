2cab3a70806cc798b3b7bcc3490731d9584a56b4
Murphy McCauley <murphy.mccauley@gmail.com>
2013-08-04 02:11:21 -0700

    openflow.util: Add DPIDWatcher
    DPIDWatcher makes it easy to just watch for events on particular DPIDs.

--- pox/openflow/util.py
+++ pox/openflow/util.py
@@ -1,4 +1,4 @@
-# Copyright 2011,2012 James McCauley
+# Copyright 2011-2013 James McCauley
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,8 @@
 
 import pox.openflow.libopenflow_01 as of
 import struct
+from pox.lib.revent import EventMixin
+import pox.openflow
 
 def make_type_to_unpacker_table ():
   """
@@ -28,3 +30,66 @@ def make_type_to_unpacker_table ():
   r = [of._message_type_to_class[i].unpack_new for i in range(0, top)]
 
   return r
+
+
+class DPIDWatcher (EventMixin):
+  """
+  Strains OpenFlow messages by DPID
+  """
+
+  #TODO: Reference count handling
+
+  _eventMixin_events = pox.openflow.OpenFlowNexus._eventMixin_events
+
+  def __init__ (self, dpid, nexus = None, invert = False):
+
+    if nexus is None:
+      from pox.core import core
+      nexus = core.openflow
+
+    self.invert = invert
+
+    self._dpids = set()
+    if isinstance(dpid, str):
+      dpid = dpid.replace(',',' ')
+      dpid = dpid.split()
+    if isinstance(dpid, (list,tuple)):
+      for d in dpid:
+        self._add_dpid(d)
+    else:
+      self._add_dpid(dpid)
+
+    #core.listen_to_dependencies(self)
+
+    for ev in self._eventMixin_events:
+      nexus.addListener(ev, self._handler)
+
+  def _handler (self, event, *args, **kw):
+    dpid = getattr(event, 'dpid', None)
+    if dpid is None:
+      return
+
+    if self.invert:
+      if event.dpid in self._dpids: return
+    else:
+      if event.dpid not in self._dpids: return
+
+    if len(args) or len(kw):
+      log.warn("Custom invoke for %s", event)
+      # This is a warning because while I think this will always or almost
+      # always work, I didn't feel like checking.
+
+    self.raiseEventNoErrors(event)
+
+  def _add_dpid (self, dpid):
+    if dpid is True:
+      # Special case -- everything!
+      self._dpids = True
+      return
+    elif self._dpids is True:
+      self._dpids = set()
+    try:
+      dpid = int(dpid)
+    except:
+      dpid = str_to_dpid(dpid)
+    self._dpids.add(dpid)
