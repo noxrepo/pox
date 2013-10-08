noxrepo/pox

commit 6097b4a795dc73bb4edb5bb550cc8c36f2962598

ioworker: Call parent constructors with super()
Previously, these were done with manual calls to the parent.

pox/lib/ioworker/workers.py
@@ -123,7 +123,8 @@ class PersistentIOWorker (RecocoIOWorker, LoggerBase):
     If the disconnect callback returns False, a new connection will NOT
     be opened.
     """
-    IOWorker.__init__(self)
+    #IOWorker.__init__(self)
+    super(PersistentIOWorker,self).__init__()
 
     self.kw = kw
 

pox/lib/ioworker/__init__.py
@@ -49,6 +49,7 @@ class IOWorker (object):
   Received data is queued until read.
   """
   def __init__(self):
+    super(IOWorker,self).__init__()
     self.send_buf = b""
     self.receive_buf = b""
     self.closed = False
@@ -355,7 +356,7 @@ class RecocoIOLoop (Task):
   more_debugging = False
 
   def __init__ (self, worker_type = RecocoIOWorker):
-    Task.__init__(self)
+    super(RecocoIOLoop,self).__init__()
     self._worker_type = worker_type
     self._workers = set()
     self.pinger = makePinger()

commit 11a13324065d69a5a3f36f61c6605e39231edcc4

ioworker: Fix PersistentIOWorker initialization
Hopefully fixes a regression introduced in 6097b4a795.  I knew that commit
was dangerous at the time I did it, which was why I left the old code in
there but commented out.  This is hopefully a reasonable fix until some
of the IOWorker stuff can get refactored.

pox/lib/ioworker/workers.py
@@ -131,7 +131,11 @@ class PersistentIOWorker (RecocoIOWorker, LoggerBase):
     be opened.
     """
     #IOWorker.__init__(self)
-    super(PersistentIOWorker,self).__init__()
+
+    # We pass None in as the socket, because we set it up in a moment in
+    # _make_connection().  This probably means that it shouldn't be
+    # a required argument for RecocoIOWorker...
+    super(PersistentIOWorker,self).__init__(None)
 
     self.kw = kw
 