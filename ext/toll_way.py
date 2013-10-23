4b164bae5bb0db6162ec9fca8d8a44d19d446371
xeonkung <xeonkung@abc.local>
2013-10-23 16:27:26 +0700

    Update toll_way.py

--- ext/toll_way.py
+++ ext/toll_way.py
@@ -1,1 +1,188 @@
-# test
+# Copyright 2013 xeonkung
+# This is Toll Way project
+#
+# This file is part of POX.
+#
+# POX is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# POX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with POX.  If not, see <http://www.gnu.org/licenses/>.
+
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.revent import *
+from pox.lib.recoco import Timer
+from collections import defaultdict
+from pox.openflow.discovery import Discovery
+from pox.lib.util import dpid_to_str
+from pox.lib.addresses import EthAddr
+import time
+
+# log
+log = core.getLogger()
+
+# Adjacency map.  [sw1][sw2] -> port from sw1 to sw2
+adjacency = defaultdict(lambda:defaultdict(lambda:None))
+
+# Switches we know of.  [dpid] -> Switch
+switches = {}
+
+# ethaddr -> (switch, port)
+mac_map = {}
+
+# [sw1][sw2] -> (distance, intermediate)
+path_map = defaultdict(lambda:defaultdict(lambda:(None,None)))
+
+# Waiting path.  (dpid,xid)->WaitingPath
+waiting_paths = {}
+
+# Time to not flood in seconds
+FLOOD_HOLDDOWN = 5
+
+# Flow timeouts
+FLOW_IDLE_TIMEOUT = 10
+FLOW_HARD_TIMEOUT = 30
+
+# How long is allowable to set up a path?
+PATH_SETUP_TIME = 4
+
+
+class Switch (EventMixin):
+  def __init__ (self):
+    self.connection = None
+    self.ports = None
+    self.dpid = None
+    self._listeners = None
+    self._connected_at = None
+
+  def __repr__ (self):
+    return dpid_to_str(self.dpid)
+
+  def disconnect (self):
+    if self.connection is not None:
+      log.debug("Disconnect %s" % (self.connection,))
+      self.connection.removeListeners(self._listeners)
+      self.connection = None
+      self._listeners = None
+
+  def connect (self, connection):
+    if self.dpid is None:
+      self.dpid = connection.dpid
+    assert self.dpid == connection.dpid
+    if self.ports is None:
+      self.ports = connection.features.ports
+    self.disconnect()
+    log.debug("Connect %s" % (connection,))
+    self.connection = connection
+    self._listeners = self.listenTo(connection)
+    self._connected_at = time.time()
+
+  @property
+  def is_holding_down (self):
+    if self._connected_at is None: return True
+    if time.time() - self._connected_at > FLOOD_HOLDDOWN:
+      return False
+    return True
+
+  def _handle_ConnectionDown (self, event):
+    self.disconnect()
+
+class toll_way (EventMixin):
+
+  # _eventMixin_events = set([
+  #   PathInstalled,
+  # ])
+
+  def __init__ (self):
+    # Listen to dependencies
+    def startup ():
+      core.openflow.addListeners(self, priority=0)
+      core.openflow_discovery.addListeners(self)
+    core.call_when_ready(startup, ('openflow','openflow_discovery'))
+
+  def _handle_LinkEvent (self, event):
+    def flip (link):
+      return Discovery.Link(link[2],link[3], link[0],link[1])
+
+    l = event.link
+    sw1 = switches[l.dpid1]
+    sw2 = switches[l.dpid2]
+
+    # Invalidate all flows and path info.
+    # For link adds, this makes sure that if a new link leads to an
+    # improved path, we use it.
+    # For link removals, this makes sure that we don't use a
+    # path that may have been broken.
+    #NOTE: This could be radically improved! (e.g., not *ALL* paths break)
+    clear = of.ofp_flow_mod(command=of.OFPFC_DELETE)
+    for sw in switches.itervalues():
+      if sw.connection is None: continue
+      sw.connection.send(clear)
+    path_map.clear()
+
+    if event.removed:
+      # This link no longer okay
+      if sw2 in adjacency[sw1]: del adjacency[sw1][sw2]
+      if sw1 in adjacency[sw2]: del adjacency[sw2][sw1]
+
+      # But maybe there's another way to connect these...
+      for ll in core.openflow_discovery.adjacency:
+        if ll.dpid1 == l.dpid1 and ll.dpid2 == l.dpid2:
+          if flip(ll) in core.openflow_discovery.adjacency:
+            # Yup, link goes both ways
+            adjacency[sw1][sw2] = ll.port1
+            adjacency[sw2][sw1] = ll.port2
+            # Fixed -- new link chosen to connect these
+            break
+    else:
+      # If we already consider these nodes connected, we can
+      # ignore this link up.
+      # Otherwise, we might be interested...
+      if adjacency[sw1][sw2] is None:
+        # These previously weren't connected.  If the link
+        # exists in both directions, we consider them connected now.
+        if flip(l) in core.openflow_discovery.adjacency:
+          # Yup, link goes both ways -- connected!
+          adjacency[sw1][sw2] = l.port1
+          adjacency[sw2][sw1] = l.port2
+
+      # If we have learned a MAC on this port which we now know to
+      # be connected to a switch, unlearn it.
+      bad_macs = set()
+      for mac,(sw,port) in mac_map.iteritems():
+        #print sw,sw1,port,l.port1
+        if sw is sw1 and port == l.port1:
+          if mac not in bad_macs:
+            log.debug("Unlearned %s", mac)
+            bad_macs.add(mac)
+        if sw is sw2 and port == l.port2:
+          if mac not in bad_macs:
+            log.debug("Unlearned %s", mac)
+            bad_macs.add(mac)
+      for mac in bad_macs:
+        del mac_map[mac]
+
+  def _handle_ConnectionUp (self, event):
+    sw = switches.get(event.dpid)
+    if sw is None:
+      # New switch
+      sw = Switch()
+      switches[event.dpid] = sw
+      sw.connect(event.connection)
+    else:
+      sw.connect(event.connection)
+
+
+def launch ():
+  core.registerNew(toll_way)
+
+  # timeout = min(max(PATH_SETUP_TIME, 5) * 2, 15)
+  # Timer(timeout, WaitingPath.expire_waiting_paths, recurring=True)
