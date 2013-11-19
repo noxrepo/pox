946f98cbe767fc7e3802df3b8108bd946970b283
Felician Nemeth <nemethf@tmit.bme.hu>
2013-11-19 10:04:20 -0800

    dns_helper: rewrite

--- pox/proto/dns_helper.py
+++ pox/proto/dns_helper.py
@@ -1,4 +1,5 @@
 # Copyright 2011-2012 James McCauley
+# Copyright 2013 Felician Nemeth
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,134 +14,169 @@
 # limitations under the License.
 
 """
-This component spies on DNS replies, stores the results, and raises events
-when things are looked up or when its stored mappings are updated.
+dns_helper hides the details of DNS communication from other modules.
 
-Similar to NOX's DNSSpy component, but with more features.
+This module follows the logic of arp_helper by hiding the wire format
+details of DNS packets and turning the into general events.
+Therefore, higher layer modules can answer DNS lookups and process DNS
+answers without understanding DNS packet formats.  Although,
+additional low level details are exposed in the events raised by
+dns_helper.
+
+TODO: option to eat_packets?
+
+Modules relying on dns_helper:
+  dns_responder
+  dns_spy
 """
 
 from pox.core import core
-import pox.openflow.libopenflow_01 as of
-import pox.lib.packet as pkt
-import pox.lib.packet.dns as pkt_dns
-
-from pox.lib.addresses import IPAddr
 from pox.lib.revent import *
+import pox.openflow.libopenflow_01 as of
+from pox.lib.addresses import IPAddr, EthAddr
+import pox.lib.packet as pkt
+from pox.lib.packet.dns import rrtype_to_str, rrclass_to_str
 
 log = core.getLogger()
 
-
-class DNSUpdate (Event):
+class DNSAnswer (Event):
   def __init__ (self, item):
-    Event.__init__()
+    super(DNSUpdate, self).__init__()
     self.item = item
 
 class DNSLookup (Event):
+  """
+  Fired with one DNS question.
+
+  When dns_helper receives a DNS packet, it fires a DNSLookup event
+  for each question it founds in the packet.  Event handlers may
+  answer this question by setting rr_answers or simple_answers.
+
+  rr_answers is a list of full "rr"s, while simple_answers is a list
+  of IP addresses for A lookups, fully qualified domain names for PTR
+  lookups, etc.
+  """
   def __init__ (self, rr):
-    Event.__init__()
+    super(DNSLookup, self).__init__()
 
     self.name = rr.name
     self.qtype = rr.qtype
 
     self.rr = rr
-    for t in pkt_dns.rrtype_to_str.values():
+    for t in rrtype_to_str.values():
       setattr(self, t, False)
-    t = pkt_dns.rrtype_to_str.get(rr.qtype)
+    t = rrtype_to_str.get(rr.qtype)
     if t is not None:
       setattr(self, t, True)
-      setattr(self, "OTHER", False)
+      setattr(self, "UNKNOWN", False)
+      self.qtype_name = t
     else:
-      setattr(self, "OTHER", True)
+      setattr(self, "UNKNOWN", True)
+      self.qtype_name = "UNKNOWN"
+    self.rr_answers = []
+    self.simple_answers = []
 
-
-class DNSSpy (EventMixin):
-  _eventMixin_events = set([ DNSUpdate, DNSLookup ])
+class DNSHelper (EventMixin):
+  _eventMixin_events = set([ DNSAnswer, DNSLookup ])
 
   def __init__ (self, install_flow = True):
     self._install_flow = install_flow
-
-    self.ip_to_name = {}
-    self.name_to_ip = {}
-    self.cname = {}
-
     core.openflow.addListeners(self)
 
-    # Add handy function to console
-    core.Interactive.variables['lookup'] = self.lookup
+  def _send_response (self, event, answers):
+    q_eth = event.parsed.find('ethernet')
+    q_ip = event.parsed.find('ipv4')
+    q_udp = event.parsed.find('udp')
+    if not (q_eth and q_ip and q_udp):
+      return
+
+    if q_udp.dstport == pkt.dns.MDNS_PORT:
+      # respose will appear to be sent from here:
+      r_ip_src = IPAddr('192.0.2.1')
+      r_eth_src = EthAddr('11:22:33:44:55:66')
+      # TODO random address is a bad idea.  We should dig up an
+      # address from the answers.
+
+      # multicast destination:
+      r_ip_dst = q_ip.dstip
+      r_eth_dst = q_eth.dst
+    else:
+      r_ip_src = q_ip.dstip
+      r_eth_src = q_eth.dst
+      r_ip_dst = q_ip.srcip
+      r_eth_dst = q_eth.src
+
+    r_dns = pkt.dns()
+    r_dns.qr = True
+    r_dns.aa = True
+    r_dns.answers = answers
+
+    r_udp = pkt.udp()
+    r_udp.srcport = q_udp.dstport
+    r_udp.dstport = q_udp.srcport
+    r_udp.payload = r_dns
+
+    r_ip = pkt.ipv4(srcip=r_ip_src, dstip=r_ip_dst)
+    r_ip.protocol = r_ip.UDP_PROTOCOL
+    r_ip.payload = r_udp
+
+    r_eth = pkt.ethernet(src=r_eth_src, dst=r_eth_dst)
+    r_eth.type = pkt.ethernet.IP_TYPE
+    r_eth.payload = r_ip
+
+    r_pkt = of.ofp_packet_out(data=r_eth.pack())
+    r_pkt.actions.append(of.ofp_action_output(port=event.port))
+
+    log.debug('response: %s' % r_dns)
+    event.connection.send(r_pkt)
 
   def _handle_ConnectionUp (self, event):
     if self._install_flow:
-      msg = of.ofp_flow_mod()
-      msg.match = of.ofp_match()
-      msg.match.dl_type = pkt.ethernet.IP_TYPE
-      msg.match.nw_proto = pkt.ipv4.UDP_PROTOCOL
-      msg.match.tp_src = 53
-      msg.actions.append(of.ofp_action_output(port = of.OFPP_CONTROLLER))
-      event.connection.send(msg)
+      def install_one_flow (event, port):
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match()
+        msg.match.dl_type = pkt.ethernet.IP_TYPE
+        msg.match.nw_proto = pkt.ipv4.UDP_PROTOCOL
+        msg.match.tp_src = port
+        msg.actions.append(of.ofp_action_output(port = of.OFPP_CONTROLLER))
+        event.connection.send(msg)
 
-  def lookup (self, something):
-    if something in self.name_to_ip:
-      return self.name_to_ip[something]
-    if something in self.cname:
-      return self.lookup(self.cname[something])
-    try:
-      return self.ip_to_name.get(IPAddr(something))
-    except:
-      return None
-
-  def _record (self, ip, name):
-    # Handle reverse lookups correctly?
-    modified = False
-    val = self.ip_to_name.setdefault(ip, [])
-    if name not in val:
-      val.insert(0, name)
-      modified = True
-
-    val = self.name_to_ip.setdefault(name, [])
-    if ip not in val:
-      val.insert(0, ip)
-      modified = True
-
-    return modified
-
-  def _record_cname (self, name, cname):
-    modified = False
-    val = self.cname.setdefault(name, [])
-    if name not in val:
-      val.insert(0, cname)
-      modified = True
-
-    return modified
+      install_one_flow(event, pkt.dns.SERVER_PORT)
+      install_one_flow(event, pkt.dns.MDNS_PORT)
 
   def _handle_PacketIn (self, event):
     p = event.parsed.find('dns')
 
-    if p is not None and p.parsed:
-      log.debug(p)
+    if p is None or not p.parsed:
+      return
+    log.debug(p)
 
-      for q in p.questions:
-        if q.qclass != 1: continue # Internet only
-        self.raiseEvent(DNSLookup, q)
+    answers = []
+    for q in p.questions:
+      if rrclass_to_str.get(q.qclass, '') != "IN":
+        continue # Internet only
+      ev = DNSLookup(q)
+      self.raiseEvent(ev)
+      for rr in ev.rr_answers:
+        answers.append(rr)
+      for rrdata in ev.simple_answers:
+        ttl = 120  # 2 minutes
+        length = 0 # should be calculated at 'packing'
+        rr = pkt.dns.rr(q.name, q.qtype, q.qclass, ttl, length, rrdata)
+        answers.append(rr)
+    if answers:
+      self._send_response(event, answers)
 
-      def process_q (entry):
-        if entry.qclass != 1:
-          # Not internet
-          return
+    def process_q (entry):
+      if rrclass_to_str.get(entry.qclass, '') != "IN":
+        return # Internet only
+      self.raiseEvent(DNSAnswer, entry)
 
-        if entry.qtype == pkt.dns.rr.CNAME_TYPE:
-          if self._record_cname(entry.name, entry.rddata):
-            self.raiseEvent(DNSUpdate, entry.name)
-            log.info("add cname entry: %s %s" % (entry.rddata, entry.name))
-        elif entry.qtype == pkt.dns.rr.A_TYPE:
-          if self._record(entry.rddata, entry.name):
-            self.raiseEvent(DNSUpdate, entry.name)
-            log.info("add dns entry: %s %s" % (entry.rddata, entry.name))
-
-      for answer in p.answers:
-        process_q(answer)
-      for addition in p.additional:
-        process_q(addition)
+    for answer in p.answers:
+      process_q(answer)
+    for addition in p.additional:
+      process_q(addition)
 
 
 def launch (no_flow = False):
-  core.registerNew(DNSSpy, not no_flow)
+  core.registerNew(DNSHelper, not no_flow)
