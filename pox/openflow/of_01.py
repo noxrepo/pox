noxrepo/pox

commit b25ad0047d271861ea74afc88268cd686e7d8038

openflow: Tweak OpenFlowHandlers init
Rather than a hard-coded list of messages the handler is interested in,
we now generate this at runtime from ofp_type_map.

Signed-off-by: Murphy McCauley <murphy.mccauley@gmail.com>

pox/openflow/of_01.py
@@ -145,23 +145,14 @@ class OpenFlowHandlers (object):
       super(OpenFlowHandlers, self)._build_table()
     except:
       pass
-    def add (n):
-      t = getattr(of, 'OFPT_' + n)
-      h = getattr(self, 'handle_' + n, None)
-      if h is None: return
-      self.add_handler(t, h)
-    add('HELLO')
-    add('ECHO_REQUEST')
-    add('ECHO_REPLY')
-    add('PACKET_IN')
-    add('FEATURES_REPLY')
-    add('PORT_STATUS')
-    add('ERROR')
-    add('BARRIER_REPLY') #FIXME: Handler name mismatch
-    add('STATS_REPLY')
-    add('FLOW_REMOVED')
-    add('VENDOR')
 
+    # Set up handlers for incoming OpenFlow messages
+    # That is, self.ofp_handlers[OFPT_FOO] = self.handle_foo
+    for type,name in of.ofp_type_map.iteritems():
+      name = name.split("OFPT_",1)[-1]
+      h = getattr(self, "handle_" + name, None)
+      if not h: continue
+      self.add_handler(type, h)
 
 class DefaultOpenFlowHandlers (OpenFlowHandlers):
   """

commit 6fe05cfee8110c90e8f9180cb6d7dcddec046f44

of_01: Fix wrong name of error handler
This addresses a regression stemming from b25ad0047 / ec2053a19 and is
based on a bug report and patch from Peter Peresini on pox-dev.  (The
original patch missed that the bug appeared in two places.)

pox/openflow/of_01.py
@@ -190,7 +190,7 @@ class DefaultOpenFlowHandlers (OpenFlowHandlers):
       con.raiseEventNoErrors(PacketIn, con, msg)
 
   @staticmethod
-  def handle_ERROR_MSG (con, msg): #A
+  def handle_ERROR (con, msg): #A
     err = ErrorIn(con, msg)
     e = con.ofnexus.raiseEventNoErrors(err)
     if e is None or e.halt != True:
@@ -272,7 +272,7 @@ class HandshakeOpenFlowHandlers (OpenFlowHandlers):
     else:
       self._finish_connecting(con)
 
-  def handle_ERROR_MSG (self, con, msg): #A
+  def handle_ERROR (self, con, msg): #A
     if not self._barrier: return
     if msg.xid != self._barrier.xid: return
     if msg.type != of.OFPET_BAD_REQUEST: return