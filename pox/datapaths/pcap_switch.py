3e8bf5b117c516d2c8d87f2569c97dec1116ec2a
Murphy McCauley <murphy.mccauley@gmail.com>
2018-01-19 21:36:48 -0800

    datapaths.pcap_switch: Add virtual ports
    This adds virtual ports to the PCap switch.  It's sort of like the OVS
    patch panel.  If you have multiple switches running in the same POX
    instance, you can use virtual ports to wire them together.
    
    When instantiating a switch from the commandline, port names starting
    with a "@" are virtual ports (the "@" is stripped from the eventual
    name).  A virtual port is connected to a channel.  If you have multiple
    virtual ports connected to the same channel, packets are passed
    between them.  If the name you pass in when instantiating the switch
    has a *second* "@" in it, the part after the second one is a
    channel name.  Thus, you could make two switches connected to each
    other with a config file like:
    
      [datapaths.pcap_switch]
      ports=@eth1@A
    
      [datapaths.pcap_switch]
      ports=@eth1,@eth2@A
    
    That connects the first switch's eth1 to the second switch's eth2 via
    a virtual channel called "A".  And the second switch has a virtual
    port called eth1 which isn't connected to anything.
    
    Note: These should really be available to other switch subclasses too.

--- pox/datapaths/pcap_switch.py
+++ pox/datapaths/pcap_switch.py
@@ -1,4 +1,4 @@
-# Copyright 2013 James McCauley
+# Copyright 2013,2018 James McCauley
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +17,33 @@ Software switch with PCap ports
 
 Example:
 ./pox.py --no-openflow datapaths.pcap_switch --address=localhost
+
+Along with using pcap to connect to real interfaces, there are virtual
+ports which can connect to each other.  This is sort of like the OVS
+patch panel.  If you have multiple switches running in the same POX
+instance, you can use virtual ports to wire them together.
+
+When instantiating a switch from the commandline, port names starting
+with a "@" are virtual ports (the "@" is stripped from the eventual
+name).  A virtual port is connected to a channel.  If you have multiple
+virtual ports connected to the same channel, packets are passed
+between them.  If the name you pass in when instantiating the switch
+has a *second* "@" in it, the part after the second one is a
+channel name.  Thus, you could make two switches connected to each
+other with a config file like:
+
+  [datapaths.pcap_switch]
+  ports=@eth1@A
+
+  [datapaths.pcap_switch]
+  ports=@eth1,@eth2@A
+
+That connects the first switch's eth1 to the second switch's eth2 via
+a virtual channel called "A".  And the second switch has a virtual
+port called eth1 which isn't connected to anything.
 """
+
+#TODO: Make virtual ports easily reusable by other switch subclasses.
 
 from pox.core import core
 from pox.datapaths import do_launch
@@ -28,6 +54,7 @@ from Queue import Queue
 from threading import Thread
 import pox.openflow.libopenflow_01 as of
 from pox.lib.packet import ethernet
+import pox.lib.packet as pkt
 import logging
 
 log = core.getLogger()
@@ -124,15 +151,101 @@ def launch (address = '127.0.0.1', port = 6633, max_retry_delay = 16,
     ports = [p for p in _ports.split(",") if p]
 
     sw = do_launch(PCapSwitch, address, port, max_retry_delay, dpid,
-                   ports=ports, extra_args=extra)
+                   ports=ports, extra_args=extra,
+                   magic_virtual_port_names = True)
     _switches[sw.name] = sw
 
   core.addListenerByName("UpEvent", up)
 
 
+class VirtualPort (object):
+  """
+  A virtual port for the PCapSwitch
+
+  It has the same interface as PCapSwitch
+  """
+  _patchbay = {} # channel_name -> list of other virtual ports
+  debug = True
+
+  def __init__ (self, switch, phy):
+    self._channel = None
+    self.phy = phy
+    self.switch = switch
+    self.started = False
+
+  @property
+  def hw_addr (self):
+    return self.phy.hw_addr
+
+  @property
+  def device (self):
+    return self.phy.name
+
+  @property
+  def port_no (self):
+    return self.phy.port_no
+
+  @property
+  def channel (self):
+    return self._channel
+
+  @channel.setter
+  def channel (self, channel):
+    if self._channel in self._patchbay:
+      # Remove from old channel
+      self._patchbay[self._channel].remove(self)
+    self._channel = channel
+    if channel is None: return
+    if channel not in self._patchbay:
+      self._patchbay[channel] = []
+    self._patchbay[channel].append(self)
+
+  @property
+  def is_link_down (self):
+    return (self.phy.state & of.OFPPS_LINK_DOWN) != 0
+
+  @is_link_down.setter
+  def is_link_down (self, value):
+    if value != self.is_link_down:
+      self.phy.state ^= of.OFPPS_LINK_DOWN
+    assert self.is_link_down == value
+
+  def _packet_hook (self, data):
+    if not self.debug: return
+    if self.channel is not None: return
+    # If there's no channel and packet is to this port, log it.
+    if isinstance(data, pkt.ethernet):
+      if self.hw_addr == data.dst:
+        log.info("%s.%s RX: %s", self.switch.name, self.phy.name, data.dump())
+
+  def inject (self, data):
+    #TODO: Support STP config?  Or does that go in the switch class?
+    if self.phy.state & of.OFPPS_LINK_DOWN: return
+
+    self._packet_hook(data)
+
+    if self._channel is None: return
+
+    data = data.pack()
+
+    for p in self._patchbay[self.channel]:
+      if p is self: continue
+      if not p.started: continue
+      if p.phy.state & of.OFPPS_LINK_DOWN: continue
+      log.debug("%s.%s -> %s.%s", self.switch.name, self.phy.name,
+                                  p.switch.name, p.phy.name)
+      p.switch._pcap_rx(p, data, 0, 0, len(data))
+
+  def start (self):
+    self.started = True
+
+
 class PCapSwitch (ExpireMixin, SoftwareSwitchBase):
   # Default level for loggers of this class
   default_log_level = logging.INFO
+
+  # If true, names starting with a "@" are virtual ports
+  magic_virtual_port_names = False
 
   def __init__ (self, **kw):
     """
@@ -143,6 +256,9 @@ class PCapSwitch (ExpireMixin, SoftwareSwitchBase):
     ports is a list of interface names
     """
     log_level = kw.pop('log_level', self.default_log_level)
+
+    self.magic_virtual_port_names = kw.pop("magic_virtual_port_names",
+                                           self.magic_virtual_port_names)
 
     self.q = Queue()
     self.t = Thread(target=self._consumer_threadproc)
@@ -167,24 +283,56 @@ class PCapSwitch (ExpireMixin, SoftwareSwitchBase):
 
     self.t.start()
 
-  def add_interface (self, name, port_no=-1, on_error=None, start=False):
+  def add_interface (self, name, port_no=-1, on_error=None, start=False,
+                     virtual=False):
+    """
+    Add an interface
+
+    This is usually a PCap interface, unless virtual is set.  If virtual
+    is True, this creates a virtual port which isn't connected to any
+    channel.  If it's a string, it's the channel name.
+    """
+    if self.magic_virtual_port_names:
+      if name.startswith("@"):
+        name = name[1:]
+        virtual = True
+        if "@" in name:
+          name,virtual = name.split("@",1)
+
     if on_error is None:
       on_error = log.error
 
-    devs = pxpcap.PCap.get_devices()
-    if name not in devs:
-      on_error("Device %s not available -- ignoring", name)
-      return
-    dev = devs[name]
-    if dev.get('addrs',{}).get('ethernet',{}).get('addr') is None:
-      on_error("Device %s has no ethernet address -- ignoring", name)
-      return
-    if dev.get('addrs',{}).get('AF_INET') != None:
-      on_error("Device %s has an IP address -- ignoring", name)
-      return
-    for no,p in self.px.iteritems():
-      if p.device == name:
-        on_error("Device %s already added", name)
+    phy = of.ofp_phy_port()
+    phy.name = name
+    # Fill in features sort of arbitrarily
+    phy.curr = of.OFPPF_10MB_HD
+    phy.advertised = of.OFPPF_10MB_HD
+    phy.supported = of.OFPPF_10MB_HD
+    phy.peer = of.OFPPF_10MB_HD
+
+    if virtual:
+      px = VirtualPort(self, phy)
+      if isinstance(virtual, str):
+        px.channel = virtual
+    else:
+      devs = pxpcap.PCap.get_devices()
+      if name not in devs:
+        on_error("Device %s not available -- ignoring", name)
+        return
+      dev = devs[name]
+      if dev.get('addrs',{}).get('ethernet',{}).get('addr') is None:
+        on_error("Device %s has no ethernet address -- ignoring", name)
+        return
+      if dev.get('addrs',{}).get('AF_INET') != None:
+        on_error("Device %s has an IP address -- ignoring", name)
+        return
+      for no,p in self.px.iteritems():
+        if p.device == name:
+          on_error("Device %s already added", name)
+
+      phy.hw_addr = dev['addrs']['ethernet']['addr']
+
+      px = pxpcap.PCap(name, callback = self._pcap_rx, start = False)
 
     if port_no == -1:
       while True:
@@ -196,21 +344,15 @@ class PCapSwitch (ExpireMixin, SoftwareSwitchBase):
       on_error("Port %s already exists -- ignoring", port_no)
       return
 
-    phy = of.ofp_phy_port()
     phy.port_no = port_no
-    phy.hw_addr = dev['addrs']['ethernet']['addr']
-    phy.name = name
-    # Fill in features sort of arbitrarily
-    phy.curr = of.OFPPF_10MB_HD
-    phy.advertised = of.OFPPF_10MB_HD
-    phy.supported = of.OFPPF_10MB_HD
-    phy.peer = of.OFPPF_10MB_HD
+    self.px[phy.port_no] = px
+
+    if virtual:
+      # We create the MAC based on the port_no, so we have to do it here
+      # and not earlier.
+      phy.hw_addr = self._gen_ethaddr(phy.port_no)
 
     self.add_port(phy)
-
-    px = pxpcap.PCap(name, callback = self._pcap_rx, start = False)
-    px.port_no = phy.port_no
-    self.px[phy.port_no] = px
 
     if start:
       px.start()
