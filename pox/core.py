1adac70d152aee4be061d5462fd1a1aa571341d2
Murphy McCauley <murphy.mccauley@gmail.com>
2018-10-05 00:09:52 -0700

    core: Update copyright date

--- pox/core.py
+++ pox/core.py
@@ -1,4 +1,4 @@
-# Copyright 2011-2017 James McCauley
+# Copyright 2011-2018 James McCauley
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -213,7 +213,7 @@ class POXCore (EventMixin):
 
   @property
   def banner (self):
-    return "{0} / Copyright 2011-2017 James McCauley, et al.".format(
+    return "{0} / Copyright 2011-2018 James McCauley, et al.".format(
      self.version_string)
 
   @property
