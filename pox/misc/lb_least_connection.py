a4a13758d581dd7226d2e2cfe53467470bd56f51
LegenDarius116 <sanagusd@bxscience.edu>
2019-09-08 17:40:54 -0400

    added scaffolding for least connection and weighted least connection lb algorithms.

---
+++ pox/misc/lb_least_connection.py
@@ -0,0 +1,19 @@
+from pox.misc.ip_load_balancer import iplb
+
+class iplb_least_connection(iplb):
+	def _pick_server(self, key, inport):
+		"""Applies least connection load balancing algorithm"""
+
+		# Find a way to get each live server's current load, then find the minimum value
+
+		raise NotImplementedError
+
+class iplb_weighted_least_connection(iplb):
+	def _pick_server(self, key, inport):
+		"""Applies weighted least connection load balancing algorithm"""
+
+		# Get each live server's current load, as well as their weights
+
+		# Get minimum load. If there are multiple servers with this load, pick the one with the highest weight among that set
+
+		raise NotImplementedError
