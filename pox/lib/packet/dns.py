28c362fdda91a70ed6eb4ee0b681203982e591dc
Matt Hall <13005813+mattall@users.noreply.github.com>
2021-05-19 14:45:50 -0700

    Made dns.py more Python3-friendly.
    Implemented the Jaisaiarun fix from issue #251 (https://github.com/noxrepo/pox/issues/251).

--- pox/lib/packet/dns.py
+++ pox/lib/packet/dns.py
@@ -397,7 +397,7 @@ class dns(packet_base):
     def read_dns_name_from_index(cls, l, index):
         retlist = []
         next = cls._read_dns_name_from_index(l, index, retlist)
-        return (next + 1, ".".join(retlist))
+        return (next + 1, b".".join(retlist))
 
     def next_rr(self, l, index, rr_list):
         array_len = len(l)
